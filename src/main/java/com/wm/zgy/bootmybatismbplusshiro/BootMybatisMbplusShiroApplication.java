51e03c1d4f37c9e5a824b139c813f91b80f47a09
renjiaxin <renjiaxin@pwrd.com>
2020-05-15 20:00:33 +0800

    配置了Druid数据源

--- src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BootMybatisMbplusShiroApplication {
 
     public static void main(String[] args) {
