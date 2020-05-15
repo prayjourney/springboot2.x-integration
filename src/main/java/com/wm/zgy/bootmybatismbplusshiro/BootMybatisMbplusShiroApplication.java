dac7d3426149fbfa457fd790559a63f76eb0f3b4
renjiaxin <renjiaxin@pwrd.com>
2020-05-15 18:23:05 +0800

    初始化仓库

---
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
@@ -0,0 +1,13 @@
+package com.wm.zgy.bootmybatismbplusshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisMbplusShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMybatisMbplusShiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wm.zgy.bootmybatismbplusshiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootMybatisMbplusShiroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
