280e080037031e9836d8d86e0b74ef06aab1ae38
renjiaxin <renjiaxin@pwrd.com>
2020-05-15 19:49:08 +0800

    配置logback日志，排除了数据源

--- src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
@@ -2,8 +2,10 @@ package com.wm.zgy.bootmybatismbplusshiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BootMybatisMbplusShiroApplication {
 
     public static void main(String[] args) {
