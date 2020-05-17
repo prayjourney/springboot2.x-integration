e000e72582117b8abdf5c5fe51ca2b90d12acdaa
zuiguangyin <2247359268@qq.com>
2020-05-17 23:25:22 +0800

    主键生成策略

--- src/main/java/com/wm/zgy/bootmybatismbplusshiro/pojo/User.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiro/pojo/User.java
@@ -1,5 +1,7 @@
 package com.wm.zgy.bootmybatismbplusshiro.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    // 主键自增策略，有uuid, 自增id, 雪花算法， redis, zookeeper
+//    @TableId(type = IdType.AUTO)
+    @TableId(type = IdType.ID_WORKER)
     private Long id;
     private String name;
     private Integer age;
