40f8f535e1ed3bbb730bc878706c2f3c5915fdd1
zuiguangyin <2247359268@qq.com>
2020-05-17 22:13:37 +0800

    所有的方法，至此都来自于BaseMapper父类，基本的CRUD都已经OKAY了，有特殊的，我们就可以去自己写了

--- src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplication.java
@@ -3,8 +3,6 @@ package com.wm.zgy.bootmybatismbplusshiro;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.wm.zgy.bootmybatismbplusshiro.mapper") // 扫描mapper文件夹
--- src/test/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplicationTests.java
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiro/BootMybatisMbplusShiroApplicationTests.java
@@ -15,11 +15,16 @@ import java.util.List;
 @SpringBootTest
 public class BootMybatisMbplusShiroApplicationTests {
 
+    /**
+     * 所有的方法，至此都来自于BaseMapper父类，基本的CRUD都已经OKAY了，有特殊的，我们就可以去自己写了
+     */
     @Autowired(required = false)
     private UserMapper userMapper;
 
     @Test
     public void contextLoads() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
     }
 
     @Test
