747075ec9bf20a2640002a54dcb8d5ccd2c6e585
renjiaxin <renjiaxin@pwrd.com>
2020-05-22 20:49:36 +0800

    通过配置文件获取值

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/ElasticSearchClientConfig.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/ElasticSearchClientConfig.java
@@ -3,6 +3,7 @@ package com.wm.zgy.bootmybatismbplusshiroesquartz.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +14,18 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ElasticSearchClientConfig {
+
+    // 通过配置文件，获取值
+    @Value("${elastic.host}")
+    private String esHost;
+
+    @Value("${elastic.port}")
+    private Integer port;
+
     @Bean
     public RestHighLevelClient restHighLevelClient() {
         RestHighLevelClient highLevelClient = new RestHighLevelClient(RestClient.builder(
-                new HttpHost("127.0.0.1", 9200, "http")
+                new HttpHost(esHost, port, "http")
         ));
         return highLevelClient;
     }
