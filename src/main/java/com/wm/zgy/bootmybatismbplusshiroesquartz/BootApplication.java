8d349eb856961886c1f7510b1d1cae54f379a3b6
renjiaxin <renjiaxin@pwrd.com>
2020-05-22 10:24:18 +0800

    添加了定时任务模块，使用@EnableScheduling开启定时任务

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/BootApplication.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/BootApplication.java
@@ -3,9 +3,11 @@ package com.wm.zgy.bootmybatismbplusshiroesquartz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.wm.zgy.bootmybatismbplusshiroesquartz.mapper") // 扫描mapper文件夹
+@EnableScheduling // 开启定时任务
 public class BootApplication {
 
     public static void main(String[] args) {
