7d1806292d4d0c42bfd361a6a5a7f8ef558b8ec6
renjiaxin <renjiaxin@pwrd.com>
2020-05-23 11:51:53 +0800

    插入数据到es的测试

--- src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/HtmlParseUtilTest.java
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/HtmlParseUtilTest.java
@@ -1,7 +1,12 @@
 package com.wm.zgy.bootmybatismbplusshiroesquartz;
 
+import com.wm.zgy.bootmybatismbplusshiroesquartz.service.ContentService;
 import com.wm.zgy.bootmybatismbplusshiroesquartz.utils.HtmlParseUtil;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
@@ -11,10 +16,22 @@ import java.io.IOException;
  * @Date: Created in 2020/5/23 1:34
  * @Modified by:
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HtmlParseUtilTest {
+    @Autowired
+    private ContentService contentService;
+
     @Test
     public void testParseJd() throws IOException {
         HtmlParseUtil htmlParseUtil = new HtmlParseUtil();
         System.out.println(htmlParseUtil.parseJd("vue"));
     }
+
+    @Test
+    public void testParseContentAdd2Es() throws Exception {
+        boolean status = contentService.parseContentAdd2Es("java");
+        System.out.println(status);
+    }
+
 }
