1323b20e9a9e0ef23be4204f32605d2c33b73893
renjiaxin <renjiaxin@pwrd.com>
2020-05-23 12:10:38 +0800

    完成了搜索功能

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/controller/JdSearchController.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/controller/JdSearchController.java
@@ -1,10 +1,16 @@
 package com.wm.zgy.bootmybatismbplusshiroesquartz.controller;
 
 import com.wm.zgy.bootmybatismbplusshiroesquartz.service.ContentService;
+import com.wm.zgy.bootmybatismbplusshiroesquartz.utils.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: renjiaxin
@@ -23,5 +29,12 @@ public class JdSearchController {
     @GetMapping({"/", "index"})
     public String helloJdSearch() {
         return "jd";
+    }
+
+    @ResponseBody
+    @GetMapping("search")
+    public String search(String keyword, int pageNo, int pageSize) throws IOException {
+        List<Map<String, Object>> vueBooks = service.searchContentWithPage(keyword, pageNo, pageSize);
+        return JSONUtil.getJsonFromObject(vueBooks);
     }
 }
