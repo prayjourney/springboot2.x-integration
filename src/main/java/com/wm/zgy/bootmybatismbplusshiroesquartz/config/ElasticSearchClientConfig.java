f8f444b6423b138eb327bea8373f4d764bd364e3
renjiaxin <renjiaxin@pwrd.com>
2020-05-29 17:13:26 +0800

    设置rest client的connection时间

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/ElasticSearchClientConfig.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/ElasticSearchClientConfig.java
@@ -1,7 +1,9 @@
 package com.wm.zgy.bootmybatismbplusshiroesquartz.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,11 +24,32 @@ public class ElasticSearchClientConfig {
     @Value("${elastic.port}")
     private Integer port;
 
+    // 使用默认参数
+    /**
     @Bean
-    public RestHighLevelClient restHighLevelClient() {
-        RestHighLevelClient highLevelClient = new RestHighLevelClient(RestClient.builder(
+    public RestHighLevelClient restHighLevelClient1() {
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(
                 new HttpHost(esHost, port, "http")
         ));
-        return highLevelClient;
+        return restHighLevelClient;
+    }
+    */
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient() {
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(esHost, port, "http")).setRequestConfigCallback(
+                new RestClientBuilder.RequestConfigCallback() {
+                    @Override
+                    public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder requestConfigBuilder) {
+                        return requestConfigBuilder
+                                .setConnectTimeout(3600000)
+                                .setConnectionRequestTimeout(3600000)
+                                .setSocketTimeout(3600000);
+                    }
+                }
+        );
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(builder);
+        return restHighLevelClient;
     }
 }
