8fdfd1d74663182f7e3bff7280672afcd759f5a5
renjiaxin <renjiaxin@pwrd.com>
2020-06-08 10:56:34 +0800

    测试不同的线程启动

---
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/utils/TestThread.java
@@ -0,0 +1,35 @@
+package com.wm.zgy.bootmybatismbplusshiroesquartz.utils;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/6/5
+ * @Description
+ */
+public class TestThread {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.activeCount());
+//        run123();
+//        while(Thread.activeCount() ==1){
+//            System.out.println("ok!");
+//            System.out.println("结束");
+//        }
+
+        new Thread(()->{
+            try {
+                Thread.sleep(5000);
+                System.out.println("你好: " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        System.out.println("hello: " + Thread.currentThread().getName());
+    }
+
+    public static void run123() throws InterruptedException {
+        Thread thread = new Thread();
+
+        thread.sleep(20000);
+        thread.start();
+    }
+}
