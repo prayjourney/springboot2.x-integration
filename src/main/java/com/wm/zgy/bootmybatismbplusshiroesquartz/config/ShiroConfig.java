fc3bebb395e0b427ca05d8f0f106c6489bcd1c9a
zuiguangyin <2247359268@qq.com>
2020-06-14 23:47:15 +0800

    配置了拦截器

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/ShiroConfig.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/ShiroConfig.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @Author: renjiaxin
  * @Despcription: 配置Shiro，层层递进，ShiroFilterFactoryBean-> DefaultWebSecurityManager-> Realm
@@ -20,6 +23,19 @@ public class ShiroConfig {
     public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultSecurityManager securityManager) {
         ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
         bean.setSecurityManager(securityManager);
+
+        // 添加shiro的内置过滤器， 在这个bean之中配置
+        /**
+         * anno:  无需认证就可访问
+         * authc: 必须认证了才能访问
+         * user:  必须拥有记住我的功能才能访问
+         * perms: 必须拥有对某个资源的全向才能访问
+         * role:  拥有某个角色权限才能访问
+         */
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        filterChainDefinitionMap.put("shiro/addmsg", "authc"); //需要认证
+        filterChainDefinitionMap.put("shiro/getmsg", "anno");  // 不需要认证
+        bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return bean;
     }
 
