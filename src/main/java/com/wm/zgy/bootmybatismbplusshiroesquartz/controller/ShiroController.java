d4f08e092d5b45b431cdeb621995d91d5a752686
zuiguangyin <2247359268@qq.com>
2020-06-15 00:51:53 +0800

    需要返回的是一个页面而非字符串，修改@RestController为@Controller

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/controller/ShiroController.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/controller/ShiroController.java
@@ -1,6 +1,7 @@
 package com.wm.zgy.bootmybatismbplusshiroesquartz.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Modified by:
  */
 @RequestMapping("/shiro")
-@RestController
+@Controller
 @Slf4j
 public class ShiroController {
 
