76fac4d75ebb81a0580ec91a254d75676c7df2d7
zuiguangyin <2247359268@qq.com>
2020-06-15 01:17:10 +0800

    登录时候的认证

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/controller/ShiroController.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/controller/ShiroController.java
@@ -1,6 +1,11 @@
 package com.wm.zgy.bootmybatismbplusshiroesquartz.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +46,8 @@ public class ShiroController {
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String loginCheck(String user, String password) {
+        // 简单的测试
+        /**
         if (null == user || null == password) {
             return "404";
         } else if ("123456".equals(password)) {
@@ -48,6 +55,21 @@ public class ShiroController {
         } else {
             return "login";
         }
+         */
+        // 获取当前的用户
+        Subject subject = SecurityUtils.getSubject();
+        // 封装用户登录的数据
+        UsernamePasswordToken token = new UsernamePasswordToken(user, password);
+        try{
+            // 执行登录方法，没有异常说明okay
+            subject.login(token);
+            return "index";
+        }catch (UnknownAccountException e1) {
+            log.error("用户名错误！ {} !", e1.getCause());
+            return "login";
+        }catch (IncorrectCredentialsException e2){
+            log.error("密码错误！ {} !", e2.getCause());
+            return "login";
+        }
     }
-
 }
