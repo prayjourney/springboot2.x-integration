28fcad9ad3ad64a534eb6cc5d99d7be1810af9ea
renjiaxin <renjiaxin@pwrd.com>
2020-06-16 11:04:58 +0800

    完成了供给型接口

--- src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/LambdaAndStream.java
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/LambdaAndStream.java
@@ -1,6 +1,7 @@
 package com.wm.zgy.bootmybatismbplusshiroesquartz;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * @Author renjiaxin
@@ -10,11 +11,18 @@ import java.util.function.Consumer;
 public class LambdaAndStream {
 
     public static void main(String[] args) {
+        //----------------消费型接口--------------------//
+        // 消费型接口: 有一个参数, 没有返回值
         // 这个定义了这个consumer要怎么做事情
         Consumer<String> consumer = str -> System.out.println(str);
         consumerMethodOld("hello");
         consumerMethodNew("hello");
         consumerMethodNew02("hello", consumer);
+
+        //----------------供给型接口--------------------//
+        // 供给型接口: 没有参数, 有一个返回值
+        System.out.println(supplierMethodOld("hello world!"));
+        System.out.println(supplierMethodNew("我草! "));
 
 
     }
@@ -45,6 +53,27 @@ public class LambdaAndStream {
     public static <T> void consumerMethodNew02(T t, Consumer<T> consumer) {
         // 这个str就是我们的消费型接口的一个参数
         consumer.accept(t);
+    }
+
+    /**
+     * 供给型：有返回值，没有参数
+     *
+     * @return
+     */
+    public static <T> String supplierMethodOld(String str) {
+        Supplier<String> supplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                return str;
+            }
+        };
+        return supplier.get();
+    }
+
+    public static <T> String supplierMethodNew(String str) {
+        // Supplier接口之中，是没有参数的
+        Supplier<String> supplier = () -> str;
+        return supplier.get();
     }
 
 }
