c57208b15a6f4606072d69325d4b21445064c243
zuiguangyin <2247359268@qq.com>
2020-06-21 12:41:38 +0800

    可重复的注解

---
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/J8RepeatableAnnotion.java
@@ -0,0 +1,82 @@
+package com.wm.zgy.bootmybatismbplusshiroesquartz;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/6/21 11:55
+ * @Modified by:
+ */
+public class J8RepeatableAnnotion {
+    public static void main(String[] args) throws NoSuchFieldException {
+        getPersonNames(PersonInfo.builder().build());
+
+    }
+
+    // 解析该对象的信息
+    public static void getPersonNames(PersonInfo p) throws NoSuchFieldException {
+        Class<PersonInfo> pClass = PersonInfo.class;
+        Method[] methods = pClass.getMethods();
+        Constructor[] constructors = pClass.getConstructors();
+        Field field = pClass.getDeclaredField("name");
+        MyAnnotation[] fieldAnnotationsByType = field.getAnnotationsByType(MyAnnotation.class);
+        for (MyAnnotation ma : fieldAnnotationsByType){
+            System.out.println(ma.value());
+        }
+
+        
+    }
+
+    @Data
+    @Builder
+    static class PersonInfo{
+        @MyAnnotation("张三")
+        @MyAnnotation("张二狗")
+        @MyAnnotation("张海涛")
+        private String name;
+
+        private Integer age;
+
+        public void show(@MyAnnotation("Hello") String name){
+            System.out.println("123");
+        }
+
+        public void getInfo(){
+            System.out.println("getInfo method!");
+        }
+    }
+}
+
+
+// 自定义的注解， 要是可重复的, 那就要用@Repeatable注解修饰，然后在其中指定注解的容器类
+@Repeatable(MyAnnotationCollect.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnnotation{
+    // 注解之中好像必须有value
+    String value() default "woshi测试的一个";
+}
+
+// 想要是可重复的，那就需要制定一个注解的容器, 这个就是MyAnnotation的容器
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnnotationCollect{
+    // 返回的是一个数组
+    MyAnnotation[] value();
+}
+
+
+
