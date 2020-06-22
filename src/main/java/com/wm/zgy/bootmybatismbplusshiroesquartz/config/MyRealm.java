26072cea67909daffab77cab29375c4d94352297
renjiaxin <renjiaxin@pwrd.com>
2020-06-22 16:31:40 +0800

    用户名密码，简单的检测

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/MyRealm.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/config/MyRealm.java
@@ -2,6 +2,8 @@ package com.wm.zgy.bootmybatismbplusshiroesquartz.config;
 
 import com.wm.zgy.bootmybatismbplusshiroesquartz.pojo.User;
 import com.wm.zgy.bootmybatismbplusshiroesquartz.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
@@ -16,12 +18,15 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 /**
  * @Author: renjiaxin
  * @Despcription: 自定義的Realm, 继承自AuthorizingRealm
  * @Date: Created in 2020/6/14 23:01
  * @Modified by:
  */
+@Slf4j
 @Component
 public class MyRealm extends AuthorizingRealm {
     @Autowired
@@ -30,34 +35,34 @@ public class MyRealm extends AuthorizingRealm {
     // 第一步: 认证，登录
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        System.out.println("登录！");
-
-        // 用户名密码， 这个本来是要在数据库之中获取，此处测试
-        // String name= "root";
-        // String password= "123456";
-
+        log.info("正在登陆的操作... 时间是: {}!", Instant.now().getNano());
+        // 此处的authenticationToken是Controller传来之后，封装好，然后传到此处的
         UsernamePasswordToken token  = (UsernamePasswordToken) authenticationToken;
         // 获取真实的数据， 连接数据库
         User user = service.queryUserByName(token.getUsername());
 
         // 没有此人
-        if (null == user){
-            return null;
-        }
-        /**
-        if (!token.getUsername().equals(this.name)){
+        if (null == user) {
             return null; // 抛出异常， unknownAccountException
         }
-        */
+        // 判断用户名密码是否不为空
+        if (null == user.getName() || StringUtils.isBlank(user.getName())) {
+            log.error("用户名不能为空");
+            return null;
+        }
+        if (null == user.getPassword() || StringUtils.isBlank(user.getPassword())) {
+            log.error("密码不能为空");
+            return null;
+        }
 
-        // 密码认证， 相当于把User返回了
+        // 上述基本的检查okay，就把他封装了，相当于把User返回了，后面就可供给登陆使用
         return new SimpleAuthenticationInfo(user, user.getPassword(), "");
     }
 
     // 第二步: 授权，访问资源
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        System.out.println("授权！");
+        log.info("正在授权的操作... 时间是: {}!", Instant.now().getNano());
         // 权限的控制
         SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
         // authorizationInfo.addStringPermission("user:add"); // 这个在实际业务之中，应该从数据库之中获取
