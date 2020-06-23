2208b06b3322d3a004a9a21850fd34b51e1abf0b
renjiaxin <renjiaxin@pwrd.com>
2020-06-23 18:04:13 +0800

    创建新文件, 支持多层级, 最后的一个是文件名, 如: d:\\1\\2\\3\\4.txt"

--- src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/utils/FileUtil.java
+++ src/main/java/com/wm/zgy/bootmybatismbplusshiroesquartz/utils/FileUtil.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Author renjiaxin
@@ -31,30 +33,50 @@ public class FileUtil {
 
     // 路径是否是多层次
     public static boolean isMultiPath(String filePath){
-        // File.separator java根据系统来判断， linux /, win \\
-        String[] split = filePath.split(File.separator);
-        if (split.length > 1){
+        // File.separator java根据系统来判断， linux /, win \
+        // File.separator使用split解析会出错: https://blog.csdn.net/jrx1995/article/details/99591897
+
+        String separator = "/|\\\\";
+        List<String> path = Arrays.asList(filePath.split(separator));
+        if (path.size()>2){
             return true;
-        }else return false;
+        } else return false;
     }
 
-    // 创建新文件
-    public static File createFile(String path) throws IOException {
+    // 创建新文件, 支持多层级, 最后的一个是文件名, 如: d:\\1\\2\\3\\4.txt"
+    public static File createFile(String filePath) throws IOException {
         // 判断路径是否为多层级
-        boolean bool = isMultiPath(path);
+        boolean bool = isMultiPath(filePath);
         if (bool) {
-            String[] split = path.split(File.separator);
-            String dir = "";
-            for (int i = 0; i < split.length - 1; i++) {
-                dir += split[i];
+            String separator = "/|\\\\";
+            List<String> pathList = Arrays.asList(filePath.split(separator));
+
+            String osName = System.getProperty("os.name");
+            String systemSeparator ="";
+            if (osName.contains("Win")){
+                systemSeparator = "\\";
+            }else {
+                systemSeparator = "/";
             }
-            File dirs = new File(dir);
+
+            String path = "";
+            for (int i = 0 ; i < pathList.size() - 1; i++) {
+                path += pathList.get(i) + systemSeparator;
+            }
+
             // 创建了多层级的目录
-            dirs.mkdirs();
+            File f = new File(path);
+            f.mkdirs();
         }
 
-        File file = new File(path);
-        if (!file.exists()) {                //如果文件不存在则新建文件
+        File file = new File(filePath);
+        //如果文件不存在则新建文件, 如果是个文件夹则也去创建
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        if (file.exists() &&file.isDirectory()){
+            file.delete();
+            log.warn("删除了原先的同名文件夹! {} !", file.getName());
             file.createNewFile();
         }
         return file;
