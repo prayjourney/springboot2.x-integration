82b1d96629a5f1978fd44b44dd109f53c0d18216
renjiaxin <renjiaxin@pwrd.com>
2020-06-23 18:06:00 +0800

    FileUtil的测试

--- src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/EasyExcelTest.java
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/EasyExcelTest.java
@@ -73,44 +73,8 @@ public class EasyExcelTest {
         EasyExcel.write(fileName, Apple.class).sheet("myapple").doWrite(cats());
     }
 
-    // 测试系统的一些信息
-    @Test
-    public void testSystem(){
-        String osName = System.getProperty("os.name");
-        String userName = System.getProperty("user.name");
-        System.out.println(osName);
-        System.out.println(userName);
-    }
+    // 简单的读
 
-    // 测试系统的分隔符
-    @Test
-    public void testFileSperator(){
-        System.out.println(File.separator);
-    }
 
-    // 创建多层级目录
-    @Test
-    public void testCreateDir(){
-        // 创建了一个3层的目录, d -> c -> 4.txt
-        String path ="d://b/c/4.txt";
-        File file = new File(path);
-        file.mkdirs();
-    }
-
-    // 测试是否为多层级的目录
-    @Test
-    public void testIsMultiPath(){
-        System.out.println(FileUtil.isMultiPath("d:\\1\\2\\3\\4.txt"));
-    }
-
-    @Test
-    public void testCreateFile01() throws IOException {
-        FileUtil.createFile("d:\\1.txt");
-    }
-
-    @Test
-    public void testCreateFile02() throws IOException {
-        FileUtil.createFile("d:\\1\\2\\3\\4.txt");
-    }
 
 }
---
+++ src/test/java/com/wm/zgy/bootmybatismbplusshiroesquartz/FileUtilTest.java
@@ -0,0 +1,60 @@
+package com.wm.zgy.bootmybatismbplusshiroesquartz;
+
+import com.wm.zgy.bootmybatismbplusshiroesquartz.utils.FileUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/6/23
+ * @Description
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileUtilTest {
+    // 测试系统的一些信息
+    @Test
+    public void testSystem(){
+        String osName = System.getProperty("os.name");
+        String userName = System.getProperty("user.name");
+        System.out.println(osName);
+        System.out.println(userName);
+    }
+
+    // 测试系统的分隔符
+    @Test
+    public void testFileSperator(){
+        System.out.println(File.separator);
+    }
+
+    // 创建多层级目录
+    @Test
+    public void testCreateDir(){
+        // 创建了一个3层的目录, d -> c -> 4.txt
+        String path ="d://b/c/4.txt";
+        File file = new File(path);
+        file.mkdirs();
+    }
+
+    // 测试是否为多层级的目录
+    @Test
+    public void testIsMultiPath(){
+        System.out.println(FileUtil.isMultiPath("d:\\1\\2\\3\\4.txt"));
+    }
+
+    @Test
+    public void testCreateFile01() throws IOException {
+        FileUtil.createFile("d:\\1.txt");
+    }
+
+    @Test
+    public void testCreateFile02() throws IOException {
+        FileUtil.createFile("d:\\1\\2\\3\\4.txt");
+    }
+
+}
