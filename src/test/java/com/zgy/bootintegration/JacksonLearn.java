8f23a413f3bdd401910ce97cb6e0ad3ad10dd63f
renjiaxin <renjiaxin@pwrd.com>
2020-07-06 15:39:34 +0800

    使用jackson的树模型, 读取jsonStr的内容

--- src/test/java/com/zgy/bootintegration/JacksonLearn.java
+++ src/test/java/com/zgy/bootintegration/JacksonLearn.java
@@ -1,7 +1,13 @@
 package com.zgy.bootintegration;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.JsonGeneratorDelegate;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.smile.SmileGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -50,6 +57,11 @@ public class JacksonLearn {
         // json file 反序列化 -> obj
         File file = new File("chinabigcity.json");
         readJSON2Obj(file, ChinaBigCity.class);
+
+        // 使用jackson树模型来读取json内容
+        String jsonString = "{\"name\":\"Mahesh Kumar\", \"age\":21,\"verified\":false,\"marks\": [100,90,85]}";
+        readTree(jsonString);
+
     }
 
     // jsonStr -> obj
@@ -91,6 +103,22 @@ public class JacksonLearn {
         Object city = mapper.readValue(file, cls);
         System.out.println(city.toString());
     }
+
+    // 使用jackson的树模型, 读取jsonStr的内容
+    public static void readTree(String str) throws JsonProcessingException {
+        // 使用JSON构建JsonNode树对象
+        JsonNode rootNode = mapper.readTree(str);
+        // 遍历Tree
+        Iterator<JsonNode> iterator = rootNode.elements();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next().asText());
+        }
+        // 直接通过关键字获取node对象
+        String jsonNodeStr = rootNode.path("name").asText();
+        System.out.println(jsonNodeStr);
+    }
+
+
 
 }
 
