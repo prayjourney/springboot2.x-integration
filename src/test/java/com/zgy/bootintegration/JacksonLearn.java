ce4feec524c10445b8099e63ef09c90c7ad99438
renjiaxin <renjiaxin@pwrd.com>
2020-07-06 11:45:21 +0800

    jackson：对象转字符串

--- src/test/java/com/zgy/bootintegration/JacksonLearn.java
+++ src/test/java/com/zgy/bootintegration/JacksonLearn.java
@@ -16,15 +16,27 @@ public class JacksonLearn {
     static ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws JsonProcessingException {
+        // str -> obj
         String jsonStr = "{\"name\": \"北京\", \"province\": \"北京\", \"postCode\": \"010\", \"area\": 192345.9}";
         str2Obj(jsonStr);
+
+        // obj -> str
+        ChinaBigCity bigCity = ChinaBigCity.builder().name("南京").province("江苏").postCode("331908").area(189345.6f).build();
+        obj2Str(bigCity);
+
+
     }
 
     // str -> obj
     public static void str2Obj(String str) throws JsonProcessingException {
         ChinaBigCity city = mapper.readValue(str, ChinaBigCity.class);
         System.out.println(city.toString());
+    }
 
+    // obj -> str
+    public static void obj2Str(Object obj) throws JsonProcessingException {
+        String str = mapper.writeValueAsString(obj);
+        System.out.println(str);
     }
 }
 
