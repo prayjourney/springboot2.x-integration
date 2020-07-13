80665094b79e8b4795b645807256535c0fc80e35
renjiaxin <renjiaxin@pwrd.com>
2020-07-13 19:53:09 +0800

    Math向上乡下取整与四舍五入

---
+++ src/test/java/com/zgy/bootintegration/MathTest.java
@@ -0,0 +1,23 @@
+package com.zgy.bootintegration;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/7/13
+ * @Description
+ */
+public class MathTest {
+    public static void main(String[] args) {
+        // floor为向下取整
+        System.out.println(Math.floor(10.2f));   // 10.0
+        System.out.println(Math.floor(10.8f));   // 10.0
+        System.out.println(Math.floor(-10.8f));  // -11.0
+        // ceil为向上取整
+        System.out.println(Math.ceil(10.3f));    // 11.0
+        System.out.println(Math.ceil(10.8f));    // 11.0
+        System.out.println(Math.ceil(-10.8f));   // -10.0
+        // round中的四舍五入函数
+        System.out.println(Math.round(10.2f));   // 10
+        System.out.println(Math.round(10.8f));   // 11
+        System.out.println(Math.round(-10.8f));  // -11
+    }
+}
