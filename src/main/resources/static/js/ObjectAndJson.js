661e2e87bd867041b7196b24ccc81d21c23bcc7b
zuiguangyin <2247359268@qq.com>
2020-07-13 22:38:11 +0800

    对象的定义和apply函数

---
+++ src/main/resources/static/js/ObjectAndJson.js
@@ -0,0 +1,40 @@
+function putongfashu(){
+    return '三十六变';
+}
+function home() {
+    return this.work
+}
+
+var sunwukong = {
+    name: '孙悟空',
+    age: 937,
+    weapon: "金箍棒",
+    fashu: function () {
+        return '七十二变';
+    }
+}
+
+var zhubajie = {
+    name: '猪八戒',
+    age: 567,
+    weapon: "九齿钉耙",
+    fashu: putongfashu
+}
+
+var shawujing = {
+    name: '沙悟净',
+    age: 736,
+    work : '天宫',
+    base :  home
+
+}
+
+var tangsanzang = {
+    name: '唐三藏',
+    age: 36,
+    work: "灵山",
+    base :  home
+}
+
+// this指向了shawujing, 如果不指向的话，那就会谁调用，指向谁
+home().apply(shawujing,[])
