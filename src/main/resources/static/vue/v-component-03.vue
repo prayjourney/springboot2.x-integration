1c57caf141eb18bd1703ccf2040e123a418296de
zuiguangyin <2247359268@qq.com>
2020-07-23 01:27:55 +0800

    两种组件的定义方式，1.原始的在html之中定义组件，2.直接创建vue组件，component

---
+++ src/main/resources/static/vue/v-component-03.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>{{item}}</li>
+</template>
+
+<script>
+    export default {
+        name: "v-componet-03",
+        props: ['item']
+    }
+</script>
+
+<style scoped>
+
+</style>
