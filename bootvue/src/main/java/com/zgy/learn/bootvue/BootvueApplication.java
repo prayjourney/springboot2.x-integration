46319c7bacf704ac24068d8aa8de6563e52fb63e
zuiguangyin <2247359268@qq.com>
2020-08-04 01:37:47 +0800

    boot+vue+bootstrap小案例

---
+++ bootvue/src/main/java/com/zgy/learn/bootvue/BootvueApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.bootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootvueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootvueApplication.class, args);
+    }
+
+}
