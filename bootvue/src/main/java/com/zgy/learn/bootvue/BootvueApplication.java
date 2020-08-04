933860041fca101a5d14e61510d90cc552091f1b
zuiguangyin <2247359268@qq.com>
2020-08-05 01:23:00 +0800

    mapper扫描

--- bootvue/src/main/java/com/zgy/learn/bootvue/BootvueApplication.java
+++ bootvue/src/main/java/com/zgy/learn/bootvue/BootvueApplication.java
@@ -1,9 +1,11 @@
 package com.zgy.learn.bootvue;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zgy.learn.bootvue.mapper") // 扫描mapper文件夹
 public class BootvueApplication {
 
     public static void main(String[] args) {
