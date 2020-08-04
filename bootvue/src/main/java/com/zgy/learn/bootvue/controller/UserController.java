8dd72b4804e5784420cb15e434b90ff427824f16
zuiguangyin <2247359268@qq.com>
2020-08-05 01:35:03 +0800

    findall的方法

--- bootvue/src/main/java/com/zgy/learn/bootvue/controller/UserController.java
+++ bootvue/src/main/java/com/zgy/learn/bootvue/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.zgy.learn.bootvue.controller;
 
+import com.zgy.learn.bootvue.pojo.User;
 import com.zgy.learn.bootvue.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @Author: renjiaxin
@@ -11,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: Created in 2020/8/5 1:25
  * @Modified by:
  */
-@RestController
+@Controller
 @RequestMapping("/")
 public class UserController {
     @Autowired
     UserService service;
+
+    @ResponseBody
+    @RequestMapping("findall")
+    public List<User> findAll() {
+        return service.findAll();
+    }
 }
