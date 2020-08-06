894d0065a33cc3aec627f1551071fdd6f4ec870c
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-08-06 19:40:33 +0800

    创建和获取cookie

--- src/main/java/com/zgy/bootintegration/controller/CookieContorller.java
+++ src/main/java/com/zgy/bootintegration/controller/CookieContorller.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,17 +20,34 @@ public class CookieContorller {
 
     /**
      *  创建cookie
-     * @param request
+     *
      * @param response
-     * @return
+     * @return str
      */
-    @GetMapping("get")
+    @GetMapping("create")
     @ResponseBody
-    public String cookie(HttpServletRequest request, HttpServletResponse response) {
+    public String cookie(HttpServletResponse response) {
         Cookie c1 = new Cookie("name", "zhangsan");
         Cookie c2 = new Cookie("hobby", "跳舞");
         response.addCookie(c1);
         response.addCookie(c2);
         return "创建cookie， 请通过F12在network之中查看";
+    }
+
+    /**
+     * 获取cookie
+     *
+     * @param request
+     * @return str
+     */
+    @GetMapping("get")
+    @ResponseBody
+    public String cookie(HttpServletRequest request) {
+        StringBuilder cookieStr = new StringBuilder();
+        Cookie[] cookies = request.getCookies();
+        for (int i = 0; i < cookies.length; i++) {
+            cookieStr.append(cookies[i].getName() +", " +cookies[i].getValue()+ "; ");
+        }
+        return "cookie的信息是: " + cookieStr.toString();
     }
 }
