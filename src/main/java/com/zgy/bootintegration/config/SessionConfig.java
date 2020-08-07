4085ab76cf7dd908ac2fa0cd86f0b9a8be0f6c33
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-08-07 17:58:41 +0800

    使用配置类的方式实现session的redis持久化

--- src/main/java/com/zgy/bootintegration/config/SessionConfig.java
+++ src/main/java/com/zgy/bootintegration/config/SessionConfig.java
@@ -6,13 +6,13 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 /**
  * @Author: renjiaxin
  * @Despcription: Spring Session的配置， 使用springboot-session处理，单位：秒，
- *                RedisFlushMode有两个参数：ON_SAVE（表示在response commit前刷新缓存），IMMEDIATE（表示只要有更新，就刷新缓存）
+ * RedisFlushMode有两个参数：ON_SAVE（表示在response commit前刷新缓存），IMMEDIATE（表示只要有更新，就刷新缓存）
  * @Date: Created in 2020/8/7 0:14
  * @Modified by:
  * @Url: https://blog.csdn.net/ljk126wy/article/details/93971421, https://www.cnblogs.com/yingsong/p/9838198.html
  */
-//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800, redisFlushMode = RedisFlushMode.ON_SAVE,
-//        redisNamespace = "springboot2.x-integration")
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800, redisFlushMode = RedisFlushMode.ON_SAVE,
+        redisNamespace = "springboot2.x-integration")
 public class SessionConfig {
 
 }
