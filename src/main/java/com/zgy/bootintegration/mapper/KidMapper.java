37bc83832add33c49ffd8795df2dbb73b9527531
zuiguangyin <2247359268@qq.com>
2020-08-09 13:11:28 +0800

    kid查询相关

---
+++ src/main/java/com/zgy/bootintegration/mapper/KidMapper.java
@@ -0,0 +1,15 @@
+package com.zgy.bootintegration.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zgy.bootintegration.pojo.Kid;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/8/9 12:50
+ * @Modified by:
+ */
+@Repository
+public interface KidMapper extends BaseMapper<Kid> {
+}
---
+++ src/main/java/com/zgy/bootintegration/pojo/Kid.java
@@ -0,0 +1,20 @@
+package com.zgy.bootintegration.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/8/9 12:49
+ * @Modified by:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Kid {
+    String id;
+    String username;
+    String password;
+}
