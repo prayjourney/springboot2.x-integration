283646d85d70f8983afbdf66bc388a42bebd3684
zuiguangyin <2247359268@qq.com>
2020-08-09 13:14:06 +0800

    KidService和TokenService

---
+++ src/main/java/com/zgy/bootintegration/service/KidService.java
@@ -0,0 +1,30 @@
+package com.zgy.bootintegration.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zgy.bootintegration.mapper.KidMapper;
+import com.zgy.bootintegration.pojo.Kid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/8/9 12:53
+ * @Modified by:
+ */
+@Service
+public class KidService {
+    @Autowired
+    KidMapper kidMapper;
+
+    public Kid findKidById(String kidId) {
+        return kidMapper.selectById(kidId);
+    }
+
+    public Kid findKidByName(String kidName) {
+        QueryWrapper queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("name", kidName);
+        return kidMapper.selectOne(queryWrapper);
+    }
+}
+
---
+++ src/main/java/com/zgy/bootintegration/service/TokenService.java
@@ -0,0 +1,29 @@
+package com.zgy.bootintegration.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.zgy.bootintegration.pojo.Kid;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: token 下发
+ * @Date: Created in 2020/8/9 12:14
+ * @Modified by:
+ */
+@Service
+public class TokenService {
+
+    public String getToken(Kid kid) {
+        Date start = new Date();
+        long currentTime = System.currentTimeMillis() + 60 * 60 * 1000;//一小时有效时间
+        Date end = new Date(currentTime);
+        String token = "";
+
+        token = JWT.create().withAudience(kid.getId()).withIssuedAt(start).withExpiresAt(end)
+                .sign(Algorithm.HMAC256(kid.getPassword()));
+        return token;
+    }
+}
