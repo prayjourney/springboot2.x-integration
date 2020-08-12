b31dd2a99e090f35dfdce7492573860b3eddf93f
zuiguangyin <2247359268@qq.com>
2020-08-12 11:00:16 +0800

    按照电话号码或者是姓名查询，条件查询的测试

--- bootvue/src/test/java/com/zgy/learn/bootvue/UserServiceTest.java
+++ bootvue/src/test/java/com/zgy/learn/bootvue/UserServiceTest.java
@@ -1,14 +1,11 @@
 package com.zgy.learn.bootvue;
 
-import com.zgy.learn.bootvue.pojo.User;
 import com.zgy.learn.bootvue.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 /**
  * @Author: renjiaxin
@@ -23,8 +20,32 @@ public class UserServiceTest {
     UserService service;
 
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         service.findAll().forEach(user -> System.out.println("user: " + user));
     }
 
+    @Test
+    public void testSelectByUserNameOrPhoneCode01() {
+        System.out.println(service.selectByUserNameOrPhoneCode("天海翼", null));
+    }
+
+    @Test
+    public void testSelectByUserNameOrPhoneCode02() {
+        System.out.println(service.selectByUserNameOrPhoneCode(null, "17788991234"));
+    }
+
+    @Test
+    public void testSelectByUserNameOrPhoneCode03() {
+        System.out.println("service.selectByUserNameOrPhoneCode(\"小泽百惠\", \"18823894589\") = " + service.selectByUserNameOrPhoneCode("小泽百惠", "18823894589"));
+    }
+
+    @Test
+    public void testSelectByUserNameOrPhoneCode04() {
+        System.out.println("service.selectByUserNameOrPhoneCode(null, null) = " + service.selectByUserNameOrPhoneCode(null, null));
+    }
+
+    @Test
+    public void testSelectByUserNameOrPhoneCode05() {
+        System.out.println("service.selectByUserNameOrPhoneCode(\"\", \"\") = " + service.selectByUserNameOrPhoneCode("", ""));
+    }
 }
