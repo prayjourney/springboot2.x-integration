434cbd6bf56490560c87e8ddffcd7d55d820c792
zuiguangyin <2247359268@qq.com>
2020-08-15 23:57:50 +0800

    跨域和添加日志记录

--- bootvue/src/main/java/com/zgy/learn/bootvue/controller/StudentController.java
+++ bootvue/src/main/java/com/zgy/learn/bootvue/controller/StudentController.java
@@ -2,7 +2,9 @@ package com.zgy.learn.bootvue.controller;
 
 import com.zgy.learn.bootvue.pojo.Student;
 import com.zgy.learn.bootvue.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.List;
  * @Date: Created in 8/15/2020 11:37 PM
  * @Modified by:
  */
+@Slf4j
 @RestController
 @RequestMapping("student")
 public class StudentController {
@@ -23,32 +26,42 @@ public class StudentController {
     private StudentService studentService;
 
     // 获取所有学生
+    @CrossOrigin
     @GetMapping("all")
     public List<Student> getAllStudent() {
+        log.info("获取所有学生");
         return studentService.getAll();
     }
 
     // 查询一个学生
+    @CrossOrigin
     @GetMapping("get")
     public Student getStudentById(Integer id) {
+        log.info("查询一个学生, id是:{} !", id);
         return studentService.getStudentById(id);
     }
 
     // 添加一个学生
+    @CrossOrigin
     @PostMapping("add")
     public Integer addStudent(Student student) {
+        log.info("添加一个学生, 学生是:{} !", student.toString());
         return studentService.addStudent(student);
     }
 
     // 删除一个学生
+    @CrossOrigin
     @GetMapping("delete")
     public Integer deleteStudentById(Integer id) {
+        log.info("删除一个学生, id是:{} !", id);
         return studentService.deleteStudentById(id);
     }
 
     // 更新一个学生
+    @CrossOrigin
     @PostMapping("update")
     public Integer updateTeacherById(Student student) {
+        log.info("更新一个学生, 学生是:{} !", student.toString());
         return studentService.updateStudentById(student);
     }
 }
--- bootvue/src/main/java/com/zgy/learn/bootvue/controller/TeacherController.java
+++ bootvue/src/main/java/com/zgy/learn/bootvue/controller/TeacherController.java
@@ -2,7 +2,9 @@ package com.zgy.learn.bootvue.controller;
 
 import com.zgy.learn.bootvue.pojo.Teacher;
 import com.zgy.learn.bootvue.service.TeacherService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.List;
  * @Date: Created in 8/15/2020 11:37 PM
  * @Modified by:
  */
+@Slf4j
 @RestController
 @RequestMapping("teacher")
 public class TeacherController {
@@ -23,32 +26,42 @@ public class TeacherController {
     private TeacherService teacherService;
 
     // 获取所有老师
+    @CrossOrigin
     @GetMapping("all")
     public List<Teacher> getAllTeacher() {
+        log.info("获取所有老师");
         return teacherService.getAll();
     }
 
     // 查询一个老师
+    @CrossOrigin
     @GetMapping("get")
     public Teacher getTeacherById(Integer id) {
+        log.info("查询一个老师， id是:{} !", id);
         return teacherService.getTeacherById(id);
     }
 
     // 添加一个老师
+    @CrossOrigin
     @PostMapping("add")
     public Integer addTeacher(Teacher teacher) {
+        log.info("添加一个老师， 老师是:{} !", teacher.toString());
         return teacherService.addTeacher(teacher);
     }
 
     // 删除一个老师
+    @CrossOrigin
     @GetMapping("delete")
     public Integer deleteTeacherById(Integer id) {
+        log.info("删除一个老师， id是:{} !", id);
         return teacherService.deleteTeacherById(id);
     }
 
     // 更新一个老师
+    @CrossOrigin
     @PostMapping("update")
     public Integer updateTeacherById(Teacher teacher) {
+        log.info("更新一个老师， 老师是:{} !", teacher.toString());
         return teacherService.updateTeacherById(teacher);
     }
 }
