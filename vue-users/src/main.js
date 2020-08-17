816d662601ac81f97db542ff5d636f491598af78
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-08-17 17:12:12 +0800

    全局引入bootrap,https://blog.csdn.net/zhangvalue/article/details/104398936

--- vue-users/src/main.js
+++ vue-users/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+// 引入bootstrap css， 全局引入：https://blog.csdn.net/zhangvalue/article/details/104398936
+import "bootstrap/dist/css/bootstrap.css"
 
 Vue.prototype.$http=axios; // 修改内部的$http为axios, $http.get("") .post
 Vue.config.productionTip = false
