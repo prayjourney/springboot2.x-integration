b280655859311338b865193da5096a500669518d
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-08-17 15:41:57 +0800

    老师添加的组件

---
+++ vue-users/src/components/TeacherAdd.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>添加老师</h2>
+    <form action="">
+      <span>老师姓名：</span><input type="text" name="name" placeholder="小龙女" v-model="teacher.name"></input><br/>
+      <span>老师年龄：</span><input type="text" name="age" placeholder="29" v-model="teacher.age"></input><br/>
+      <span>老师学历：</span><input type="text" name="grade" placeholder="本科" v-model="teacher.grade"></input><br/>
+      <input type="submit" value="提交" style="margin: 10px; " @click="saveTeacher(teacher)">
+      <input type="submit" value="取消" style="margin: 10px; " @click="reset">
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TeacherAdd",
+    data() {
+      return {
+        teacher: ""
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
