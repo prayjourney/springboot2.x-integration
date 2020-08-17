161878f18b01dfcec4e379d83a5cd2b7b9c01351
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-08-17 15:47:05 +0800

    给学生添加子组件添加子路由

--- vue-users/src/router/index.js
+++ vue-users/src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../components/Home"
 import Teacher from "../components/Teacher";
 import Student from "../components/Student";
 import TeacherAdd from "../components/TeacherAdd";
+import StudentAdd from "../components/StudentAdd";
 
 Vue.use(Router)
 
@@ -14,11 +15,14 @@ export default new Router({
     {
       path: '/teacher', name: 'teacher', component: Teacher,
       children: [
-        {path: '/add', name: 'add', component: TeacherAdd}
+        {path: '/add', name: 'teacheradd', component: TeacherAdd}
       ]
     },
-    {path: '/student', name: 'student', component: Student},
-    // {path: '/add', name: 'teacheradd', component: TeacherAdd}
-
+    {
+      path: '/student', name: 'student', component: Student,
+      children: [
+        {path: '/add', name: 'studentadd', component: StudentAdd}
+      ]
+    }
   ]
 })
