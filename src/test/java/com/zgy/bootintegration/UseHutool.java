7df8baa1862d7b059443d0be52de15b4c2dde9fc
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-08-17 11:30:13 +0800

    简单的转换String和Date之间的转换

---
+++ src/test/java/com/zgy/bootintegration/UseHutool.java
@@ -0,0 +1,36 @@
+package com.zgy.bootintegration;
+
+import cn.hutool.Hutool;
+import cn.hutool.core.convert.impl.DateConverter;
+import cn.hutool.core.convert.impl.StringConverter;
+import cn.hutool.core.date.LocalDateTimeUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/8/17
+ * @Description
+ */
+public class UseHutool {
+    public static void main(String[] args) {
+        String date1 = "2020-9-18";
+        String date2 = "2020-09-18";
+        String date3 = "2020/9/18";
+
+        // String转换为Date
+        DateConverter dateConverter01 = new DateConverter(Date.class);
+        System.out.println(dateConverter01.convert(date1, new Date()));
+        System.out.println(dateConverter01.convert(date2, new Date()));
+        System.out.println(dateConverter01.convert(date3, new Date()));
+        System.out.println(LocalDateTimeUtil.of(new Date()));
+
+        // Date转成String
+        LocalDateTime now = LocalDateTime.now();
+        StringConverter stringConverter = new StringConverter();
+        System.out.println(stringConverter.convert(now, ""));
+
+    }
+}
