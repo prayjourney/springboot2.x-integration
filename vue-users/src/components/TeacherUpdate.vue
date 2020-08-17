0c695696725022b9f63641911f4043852fd8aa9f
zuiguangyin <2247359268@qq.com>
2020-08-18 01:28:44 +0800

    更新准备，获取信息，双向绑定

--- vue-users/src/components/TeacherUpdate.vue
+++ vue-users/src/components/TeacherUpdate.vue
@@ -3,10 +3,10 @@
     <h2>更新老师</h2>
     <!-- 改成button，然后这儿就不乱跳了，感觉好像是只有formzi自己带了action的时候，input才设置为submit,如果是异步的方式，都设置成为button -->
     <form action="" class="form-inline">
-      <span>id：</span><input type="text" v-model="teacher.id" readonly></input>
-      <span>姓名：</span><input type="text"   v-model="teacher.name"></input>
-      <span>年龄：</span><input type="text"   v-model="teacher.age"></input>
-      <span>学历：</span><input type="text"  v-model="teacher.grade"></input>
+      <span>id：</span> <input type="text" v-model="teacher.id" readonly></input>
+      <span>姓名：</span><input type="text" v-model="teacher.name"></input>
+      <span>年龄：</span><input type="text" v-model="teacher.age"></input>
+      <span>学历：</span><input type="text" v-model="teacher.grade"></input>
       <input type="button" value="提交" class="btn btn-success btn-sm" @click="updateTeacher(teacher)">&nbsp;&nbsp;
       <input type="reset" value="取消" class="btn btn-primary btn-sm" @click="reset">
     </form>
@@ -18,7 +18,10 @@
     name: "TeacherUpdate",
     data() {
       return {
-        teacher: ""
+        teacher: {
+          // 表示id属性一定有
+          id : ""
+        }
       }
     },
     methods: {
@@ -42,7 +45,20 @@
 
       reset() {
         this.teacher = "";
+      },
+
+      getTeacherById(){
+        this.$http.get('http://localhost:8988/bootvue/teacher/get?id=' + this.teacher.id).then(function (resp) {
+          // TeacherUpdate.vue?bc81:52 Uncaught (in promise) TypeError: Cannot set property 'teacher' of undefined
+          this.teacher = resp.data
+        })
       }
+    },
+    // 此处就可以接收路由传参的id了
+    created() {
+      console.log("修改的老师的id: " + this.$route.query.id)
+      this.teacher.id =this.$route.query.id;
+      this.getTeacherById();
     }
   }
 </script>
