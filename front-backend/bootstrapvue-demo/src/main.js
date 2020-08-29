c0bc42fbc5cd4567c1df01c86caa5b7e07a21259
zuiguangyin <2247359268@qq.com>
2020-08-30 02:26:36 +0800

    引入bootstrapvue的组件，样式

--- front-backend/bootstrapvue-demo/src/main.js
+++ front-backend/bootstrapvue-demo/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入bootstrapvue的组件，样式
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
