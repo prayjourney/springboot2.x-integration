0ad573d9fe32d7acda750359a1d8950aba34ef04
zuiguangyin <2247359268@qq.com>
2020-08-30 02:36:46 +0800

    添加组件，然后放入到路由之中

---
+++ front-backend/bootstrapvue-demo/src/components/MyComponent001.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-button size="sm" class="my-2 my-1 btn-info">hello</b-button>
+      </b-row>
+    </b-container>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "MyComponent001"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- front-backend/bootstrapvue-demo/src/router/index.js
+++ front-backend/bootstrapvue-demo/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MyComponent001 from "../components/MyComponent001";
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/my',
+      name: 'MyComponent001',
+      component: MyComponent001
     }
   ]
 })
