5cf8885d5d71534e6aaf157ae19d456e92b85423
zuiguangyin <2247359268@qq.com>
2020-08-30 16:39:36 +0800

    基本的导航栏

--- front-backend/bootstrapvue-demo/src/components/NavComponent.vue
+++ front-backend/bootstrapvue-demo/src/components/NavComponent.vue
@@ -1,11 +1,23 @@
 <template>
+  <div>
+    <div class="container">
+      <div class="row col col-10 offset-1">
+        <b-nav tabs>
+          <b-nav-item active>Active</b-nav-item>
+          <b-nav-item>Link</b-nav-item>
+          <b-nav-item>Another Link</b-nav-item>
+          <b-nav-item disabled>Disabled</b-nav-item>
+        </b-nav>
+      </div>
+    </div>
+  </div>
 
 </template>
 
 <script>
-    export default {
-        name: "NavComponent"
-    }
+  export default {
+    name: "NavComponent"
+  }
 </script>
 
 <style scoped>
