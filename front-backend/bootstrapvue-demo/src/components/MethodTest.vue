9122d8e3ddf089758c7412e4167560dd00c76621
zuiguangyin <2247359268@qq.com>
2020-08-30 14:55:11 +0800

    三个组件

--- front-backend/bootstrapvue-demo/src/components/MethodTest.vue
+++ front-backend/bootstrapvue-demo/src/components/MethodTest.vue
@@ -11,7 +11,7 @@
 
 <script>
   export default {
-    name: "MyComponent001",
+    name: "MethodTest",
     data() {
 
     },
---
+++ front-backend/bootstrapvue-demo/src/components/NavComponent.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "NavComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
