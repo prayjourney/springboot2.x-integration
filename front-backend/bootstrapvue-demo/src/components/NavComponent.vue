1851ee4b2b8c9c3304ccd36d261b0cbe6449aa90
zuiguangyin <2247359268@qq.com>
2020-08-30 17:05:21 +0800

    下拉菜单导航

--- front-backend/bootstrapvue-demo/src/components/NavComponent.vue
+++ front-backend/bootstrapvue-demo/src/components/NavComponent.vue
@@ -19,6 +19,37 @@
           <b-nav-item disabled>关于</b-nav-item>
         </b-nav>
       </div>
+
+      <!-- 下拉菜单导航 -->
+      <span class="lead text-info text-center"> 充满的所占页面的一个导航 </span>
+      <div>
+        <b-nav tabs fill>
+          <b-nav-item active>首页</b-nav-item>
+          <b-nav-item>新闻</b-nav-item>
+          <b-nav-item-dropdown id="mynavitem" text="故事" toggle-class="nav-link-custom">
+            <b-dropdown-item>呐喊</b-dropdown-item>
+            <b-dropdown-item>狂人日记</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item>阿Q正传</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item disabled>关于</b-nav-item>
+        </b-nav>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
     </div>
 
   </div>
