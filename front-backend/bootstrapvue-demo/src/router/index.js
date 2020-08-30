5eea4c9918acfb838713c0e1588ff9b597ff825d
zuiguangyin <2247359268@qq.com>
2020-08-30 14:49:55 +0800

    删除了脚手架带的组件

--- front-backend/bootstrapvue-demo/src/router/index.js
+++ front-backend/bootstrapvue-demo/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import MyComponent001 from "../components/MyComponent001";
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/my',
       name: 'MyComponent001',
       component: MyComponent001
     }
