2452889175e41be2b9896c1337c3430e724381df
zuiguangyin <2247359268@qq.com>
2020-08-30 16:49:15 +0800

    充满的所占页面的一个导航

--- front-backend/bootstrapvue-demo/src/components/NavComponent.vue
+++ front-backend/bootstrapvue-demo/src/components/NavComponent.vue
@@ -9,7 +9,18 @@
           <b-nav-item disabled>Disabled</b-nav-item>
         </b-nav>
       </div>
+      <!-- 充满的所占页面的一个导航 -->
+      <span class="lead text-info text-center"> 充满的所占页面的一个导航 </span>
+      <div>
+        <b-nav tabs fill>
+          <b-nav-item active>首页</b-nav-item>
+          <b-nav-item>新闻</b-nav-item>
+          <b-nav-item>故事</b-nav-item>
+          <b-nav-item disabled>关于</b-nav-item>
+        </b-nav>
+      </div>
     </div>
+
   </div>
 
 </template>
