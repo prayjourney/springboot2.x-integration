17eec7814122f4b636c0457007f05f148331fe15
zuiguangyin <2247359268@qq.com>
2020-10-08 23:19:18 +0800

    🍿🍿🍿大文件上传

---
+++ src/main/java/com/zgy/bootintegration/controller/BigFileUpload.java
@@ -0,0 +1,13 @@
+package com.zgy.bootintegration.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription: 大文件上传
+ * @Date: Created in 2020/10/8 23:18
+ * @Modified by:
+ */
+@Controller
+public class BigFileUpload {
+}
