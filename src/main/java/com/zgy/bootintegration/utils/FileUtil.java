4eb83c4b34f269b53e22f30412d20602fed08c23
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-13 11:33:49 +0800

    🦺🦺🦺格式化

--- src/main/java/com/zgy/bootintegration/utils/FileUtil.java
+++ src/main/java/com/zgy/bootintegration/utils/FileUtil.java
@@ -18,29 +18,32 @@ import java.util.List;
 @Slf4j
 @Component
 public class FileUtil {
-
     // 文件是否存在
-    public static boolean fileExists(String filePath){
-        if (!StringUtils.isBlank(filePath)){
+    public static boolean fileExists(String filePath) {
+        if (!StringUtils.isBlank(filePath)) {
             File file = new File(filePath);
-            if (file.exists()) return true;
-            else return false;
-        }else{
+            if (file.exists()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
             log.error("文件不存在, 文件的路径是: {}! ", filePath);
-            return  false;
+            return false;
         }
     }
 
     // 路径是否是多层次
-    public static boolean isMultiPath(String filePath){
+    public static boolean isMultiPath(String filePath) {
         // File.separator java根据系统来判断， linux /, win \
         // File.separator使用split解析会出错: https://blog.csdn.net/jrx1995/article/details/99591897
-
         String separator = "/|\\\\";
         List<String> path = Arrays.asList(filePath.split(separator));
-        if (path.size()>2){
+        if (path.size() > 2) {
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
 
     // 创建新文件, 支持多层级, 最后的一个是文件名, 如: d:\\1\\2\\3\\4.txt"
@@ -52,15 +55,15 @@ public class FileUtil {
             List<String> pathList = Arrays.asList(filePath.split(separator));
 
             String osName = System.getProperty("os.name");
-            String systemSeparator ="";
-            if (osName.contains("Win")){
+            String systemSeparator = "";
+            if (osName.contains("Win")) {
                 systemSeparator = "\\";
-            }else {
+            } else {
                 systemSeparator = "/";
             }
 
             String path = "";
-            for (int i = 0 ; i < pathList.size() - 1; i++) {
+            for (int i = 0; i < pathList.size() - 1; i++) {
                 path += pathList.get(i) + systemSeparator;
             }
 
@@ -70,11 +73,11 @@ public class FileUtil {
         }
 
         File file = new File(filePath);
-        //如果文件不存在则新建文件, 如果是个文件夹则也去创建
+        // 如果文件不存在则新建文件, 如果是个文件夹则也去创建
         if (!file.exists()) {
             file.createNewFile();
         }
-        if (file.exists() &&file.isDirectory()){
+        if (file.exists() && file.isDirectory()) {
             file.delete();
             log.warn("删除了原先的同名文件夹! {} !", file.getName());
             file.createNewFile();
@@ -90,7 +93,8 @@ public class FileUtil {
         }
         FileOutputStream output = new FileOutputStream(f);
         byte[] bytes = content.getBytes("UTF-8");
-        output.write(bytes);    //将数组的信息写入文件中
+        // 将数组的信息写入文件中
+        output.write(bytes);
         output.close();
     }
 }
