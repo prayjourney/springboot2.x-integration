3b5987566beea3c7023b15eaa56971621452910f
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-13 11:42:47 +0800

    🥞🥞🥞修改上传文件的pojo

--- src/main/java/com/zgy/bootintegration/pojo/MultipartFileParam.java
+++ src/main/java/com/zgy/bootintegration/pojo/MultipartFileParam.java
@@ -22,22 +22,25 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @Accessors(chain = true)
 public class MultipartFileParam {
-    // 文件传输任务ID
-    private String taskId;
+    // 用户id
+    private String uid;
 
-    // 当前为第几分片
+    // 任务ID
+    private String id;
+
+    // 总分片数量
+    private int chunks;
+
+    // 当前为第几块分片
     private int chunk;
 
-    // 每个分块的大小
-    private long size;
+    // 当前分片大小
+    private long size = 0L;
 
-    // 分片总数
-    private int chunkTotal;
+    // 文件名
+    private String name;
 
-    // 主体类型--这个字段是我项目中的其他业务逻辑可以忽略
-    private int objectType;
-
-    // 分块文件传输对象
+    // 分片对象
     private MultipartFile file;
 
     // MD5值
