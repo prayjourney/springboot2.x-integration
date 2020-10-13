aa3adac7de285ba8d44da5e492653fe348f9c4a1
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-13 11:25:24 +0800

    ⛸⛸⛸结果的封装

---
+++ src/main/java/com/zgy/bootintegration/vo/ResultVo.java
@@ -0,0 +1,54 @@
+package com.zgy.bootintegration.vo;
+
+import lombok.ToString;
+
+/**
+ * 统一返回结果pojo
+ */
+@ToString
+public class ResultVo<T> {
+
+    private ResultStatus status;
+
+    private String msg;
+
+    private T data;
+
+    public ResultVo(ResultStatus status) {
+        this(status, status.getReasonPhrase(), null);
+    }
+
+    public ResultVo(ResultStatus status, T data) {
+        this(status, status.getReasonPhrase(), data);
+    }
+
+    public ResultVo(ResultStatus status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public ResultStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ResultStatus status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
