ddb826a5622c21bc4f10c6d6d70ca69c59678e50
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-13 11:06:17 +0800

    🎈🎈🎈POJO类

---
+++ src/main/java/com/zgy/bootintegration/pojo/FileMd5.java
@@ -0,0 +1,35 @@
+package com.zgy.bootintegration.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/12
+ * @Description
+ */
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class FileMd5 {
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String fileMd5;
+    private String filePath;
+}
+
+
+
+
+
