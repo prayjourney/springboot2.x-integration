a0f5c5ca4985fd9129c1993fc6cf255cfc883851
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-19 16:15:14 +0800

    🪕🪕🪕干净的springboot测试工程

---
+++ clean-env/src/main/java/com/zgy/learn/cleanenv/CleanEnvApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.cleanenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanEnvApplication.class, args);
+    }
+
+}
---
+++ clean-env/src/test/java/com/zgy/learn/cleanenv/CleanEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.cleanenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanEnvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
