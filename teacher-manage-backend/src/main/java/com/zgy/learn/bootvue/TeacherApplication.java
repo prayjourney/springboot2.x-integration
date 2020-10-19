191c9fa198a3fcbf3f41ac47ec5a902fbdf84a0c
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-19 16:55:06 +0800

    🀄🀄🀄修改类名

--- teacher-manage-backend/src/main/java/com/zgy/learn/bootvue/TeacherApplication.java
+++ teacher-manage-backend/src/main/java/com/zgy/learn/bootvue/TeacherApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.zgy.learn.bootvue.mapper") // 扫描mapper文件夹
-public class BootvueApplication {
+public class TeacherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootvueApplication.class, args);
+        SpringApplication.run(TeacherApplication.class, args);
     }
 
 }
