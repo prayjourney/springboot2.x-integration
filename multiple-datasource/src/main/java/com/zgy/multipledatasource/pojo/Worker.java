e679255225030ffed9d82f9f85584538fd0fbb76
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 11:59:29 +0800

    🧉🧉🧉pojo类

---
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/pojo/Worker.java
@@ -0,0 +1,51 @@
+package com.zgy.multipledatasource.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/20
+ * @Description
+ */
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Worker {
+    /**
+     * 工人id
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 性别：0-男，1-女
+     */
+    private Integer sex;
+
+    /**
+     * 地址
+     */
+    private String addr;
+}
