08c22ddd192a1f9af96e1d67f5bc3866e79ec7ac
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 19:15:04 +0800

    💦💦💦POJO

---
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/pojo/Scenery.java
@@ -0,0 +1,56 @@
+package com.zgy.multipledatasource.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/20
+ * @Description
+ */
+@Setter
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Scenery {
+    /**
+     * 景色id
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 国家
+     */
+    private String country;
+
+    /**
+     * 城市
+     */
+    private String city;
+
+    /**
+     * 面积
+     */
+    private Integer area;
+
+    /**
+     * 地址
+     */
+    private String addr;
+}
