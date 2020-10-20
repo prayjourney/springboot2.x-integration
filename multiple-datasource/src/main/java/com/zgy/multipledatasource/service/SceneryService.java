39112f13093bc92a90a82e75ac76d270ac91e36a
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 16:05:51 +0800

    🧇🧇🧇服务

--- multiple-datasource/src/main/java/com/zgy/multipledatasource/service/SceneryService.java
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/service/SceneryService.java
@@ -1,6 +1,9 @@
 package com.zgy.multipledatasource.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zgy.multipledatasource.mapper.SceneryMapper;
+import com.zgy.multipledatasource.pojo.Scenery;
+import com.zgy.multipledatasource.pojo.Worker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +19,21 @@ public class SceneryService {
     @Autowired
     private SceneryMapper sceneryMapper;
 
+    public Scenery selectSceneryById(Integer id) {
+        return sceneryMapper.selectById(id);
+    }
 
+    public int deleteSceneryById(Integer id) {
+        return sceneryMapper.deleteById(id);
+    }
+
+    public String selectSceneryCountryById(Integer id) {
+        return sceneryMapper.selectById(id).getCountry();
+    }
+
+    public String getSceneryNameById(Integer id) {
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id", id);
+        return sceneryMapper.selectOne(wrapper).getName();
+    }
 }
--- multiple-datasource/src/main/java/com/zgy/multipledatasource/service/WorkerService.java
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/service/WorkerService.java
@@ -1,6 +1,8 @@
 package com.zgy.multipledatasource.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zgy.multipledatasource.mapper.WorkerMapper;
+import com.zgy.multipledatasource.pojo.Worker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +19,17 @@ public class WorkerService {
     @Autowired(required = false)
     private WorkerMapper workerMapper;
 
+    public Worker selectWorkerById(Integer id) {
+        return workerMapper.selectById(id);
+    }
+
+    public int deleteWorkerById(Integer id) {
+        return workerMapper.deleteById(id);
+    }
+
+    public String getWorkerNameById(Integer id) {
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id", id);
+        return workerMapper.selectOne(wrapper).getName();
+    }
 }
