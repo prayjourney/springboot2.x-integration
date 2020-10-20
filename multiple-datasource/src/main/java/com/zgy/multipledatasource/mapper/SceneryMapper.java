dd0b107c89026f1fdd979ef7d4d85a138f6a7870
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 13:11:42 +0800

    🧇🧇🧇mapper接口

---
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/mapper/SceneryMapper.java
@@ -0,0 +1,12 @@
+package com.zgy.multipledatasource.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zgy.multipledatasource.pojo.Scenery;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/20
+ * @Description
+ */
+public interface SceneryMapper extends BaseMapper<Scenery> {
+}
---
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/mapper/WorkerMapper.java
@@ -0,0 +1,12 @@
+package com.zgy.multipledatasource.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zgy.multipledatasource.pojo.Worker;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/20
+ * @Description
+ */
+public interface WorkerMapper extends BaseMapper<Worker> {
+}
