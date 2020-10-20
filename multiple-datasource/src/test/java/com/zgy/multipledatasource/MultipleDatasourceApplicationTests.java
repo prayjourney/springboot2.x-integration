bf448b972c3005b3b08472a39c3c2f4605ed423a
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 19:26:01 +0800

    💌💌💌单元测试

--- multiple-datasource/src/test/java/com/zgy/multipledatasource/MultipleDatasourceApplicationTests.java
+++ multiple-datasource/src/test/java/com/zgy/multipledatasource/MultipleDatasourceApplicationTests.java
@@ -1,10 +1,10 @@
 package com.zgy.multipledatasource;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MultipleDatasourceApplicationTests {
+public class MultipleDatasourceApplicationTests {
 
     @Test
     void contextLoads() {
