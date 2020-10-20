eb421190a8d38ddf942bc0dc86a168f58f6c9e49
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 18:14:55 +0800

    💦💦💦两个数据源的配置

---
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/config/DB2DataSourceConfig.java
@@ -0,0 +1,43 @@
+package com.zgy.multipledatasource.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/20
+ * @Description
+ */
+@Configuration
+@MapperScan(basePackages = "com.zgy.multipledatasource.mapper.db2", sqlSessionFactoryRef = "db2SqlSessionFactory")
+public class DB2DataSourceConfig {
+
+    @Bean("db2DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.db2")
+    public DataSource getDb1DataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("db2SqlSessionFactory")
+    public SqlSessionFactory db1SqlSessionFactory(@Qualifier("db2DataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/db2/*.xml"));
+        return bean.getObject();
+    }
+
+    @Bean("db2SqlSessionTemplate")
+    public SqlSessionTemplate db1SqlSessionTemplate(@Qualifier("db2SqlSessionFactory") SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
