8f6601b8aa33e5c93f6ca8f8cb0361ea6de2a2b6
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-20 11:48:07 +0800

    🌱🌱🌱配置文件

--- multiple-datasource/src/main/java/com/zgy/multipledatasource/MultipleDatasourceApplication.java
+++ multiple-datasource/src/main/java/com/zgy/multipledatasource/MultipleDatasourceApplication.java
@@ -3,6 +3,7 @@ package com.zgy.multipledatasource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @SpringBootApplication
 public class MultipleDatasourceApplication {
 
