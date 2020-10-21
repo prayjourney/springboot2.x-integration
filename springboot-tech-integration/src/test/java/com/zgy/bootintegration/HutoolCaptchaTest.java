1bcd0be353ed13a033787401ec16db097700fa40
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-21 15:44:46 +0800

    🌱🌱🌱使用hutool工具集合生成验证码

---
+++ springboot-tech-integration/src/test/java/com/zgy/bootintegration/HutoolCaptchaTest.java
@@ -0,0 +1,32 @@
+package com.zgy.bootintegration;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.core.lang.Console;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/10/21
+ * @Description
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HutoolCaptchaTest {
+
+    @Test
+    public void testCreateCodeImage(){
+        //定义图形验证码的长和宽
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
+
+        //图形验证码写出，可以写出到文件，也可以写出到流
+        lineCaptcha.write("d:/line.png");
+        //输出code
+        Console.log(lineCaptcha.getCode());
+        //验证图形验证码的有效性，返回boolean值
+        lineCaptcha.verify("1234");
+    }
+}
