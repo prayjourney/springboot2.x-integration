95e3f73e5e5d55fa0c5ddc3a98d872eae2bc25b4
zuiguangyin <2247359268@qq.com>
2020-10-25 19:14:11 +0800

    🥙🥙🥙kid的实体和查询的mapper

--- springboot-tech-integration/src/main/java/com/zgy/bootintegration/mapper/KidMapper.java
+++ springboot-tech-integration/src/main/java/com/zgy/bootintegration/mapper/KidMapper.java
@@ -2,6 +2,7 @@ package com.zgy.bootintegration.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zgy.bootintegration.pojo.Kid;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
  * @Date: Created in 2020/8/9 12:50
  * @Modified by:
  */
+@Mapper
 @Repository
 public interface KidMapper extends BaseMapper<Kid> {
 }
