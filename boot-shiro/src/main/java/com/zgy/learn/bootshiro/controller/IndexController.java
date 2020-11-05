7ec7fb7f8e6ee842eeb474d56e8d8ce8c528fca8
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-11-05 17:26:41 +0800

    🧦🧦🧦添加了add和update页面

--- boot-shiro/src/main/java/com/zgy/learn/bootshiro/controller/IndexController.java
+++ boot-shiro/src/main/java/com/zgy/learn/bootshiro/controller/IndexController.java
@@ -11,9 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+
     @RequestMapping({"", "/", "home", "index"})
     public String index(Model model) {
         model.addAttribute("message", "这是来自后端的测试消息!");
         return "index";
+    }
+
+
+    @RequestMapping("user/add")
+    public String add() {
+        return "user/add";
+
+    }
+
+
+    @RequestMapping("user/update")
+    public String update() {
+        return "user/update";
+
     }
 }
