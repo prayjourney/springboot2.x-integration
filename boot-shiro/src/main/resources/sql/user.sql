3fcd8cf158363b1bc391653c0e8201b3bebed471
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-11-06 11:07:40 +0800

    🧤🧤🧤修改数据库连接, 修改mapper, service等

--- boot-shiro/src/main/resources/sql/user.sql
+++ boot-shiro/src/main/resources/sql/user.sql
@@ -4,22 +4,22 @@ Navicat MySQL Data Transfer
 Source Server         : local-mysql
 Source Server Version : 50505
 Source Host           : localhost:3306
-Source Database       : ourschool
+Source Database       : security
 
 Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2020-11-06 00:33:49
+Date: 2020-11-06 10:38:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for person
+-- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `person`;
-CREATE TABLE `person` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
@@ -27,8 +27,8 @@ CREATE TABLE `person` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of person
+-- Records of user
 -- ----------------------------
-INSERT INTO `person` VALUES ('1', 'admin', '123456');
-INSERT INTO `person` VALUES ('2', 'zhangsan', 'abc123');
-INSERT INTO `person` VALUES ('3', '小明', '111222');
+INSERT INTO `user` VALUES ('1', 'admin', '123456');
+INSERT INTO `user` VALUES ('2', 'zhangsan', 'abc123');
+INSERT INTO `user` VALUES ('3', '小明', '111222');
