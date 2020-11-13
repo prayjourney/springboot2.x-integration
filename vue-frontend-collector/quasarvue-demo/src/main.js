06cba630d9214859d28a168d43f346e4b4617eee
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-11-13 16:53:14 +0800

    🧨🧨🧨还是使用vue2.9.6这个版本

--- vue-frontend-collector/quasarvue-demo/src/main.js
+++ vue-frontend-collector/quasarvue-demo/src/main.js
@@ -6,10 +6,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
