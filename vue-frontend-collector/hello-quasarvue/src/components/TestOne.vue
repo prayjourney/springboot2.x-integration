87fd74b0421a77469b6506dea572b06275c90fde
zuiguangyin <2247359268@qq.com>
2020-11-14 01:11:28 +0800

    🏀🏀🏀传参数

--- vue-frontend-collector/hello-quasarvue/src/components/TestOne.vue
+++ vue-frontend-collector/hello-quasarvue/src/components/TestOne.vue
@@ -3,6 +3,9 @@
         <h1>{{ msg }}</h1>
         <h2 v-text="text"></h2>
         <h2 v-html="info"></h2>
+        <input type="button" value="点击啊！" v-on:click="helloClick" />
+        <input id="hello" type="button" value="点击啊！" @click="helloClick" />
+        <input id="hello" type="button" value="点击啊！" @click="clickMethod('你好')" />
     </div>
 </template>
 
@@ -13,15 +16,24 @@ export default {
         return {
             msg: "Test One",
             text: "我有一个梦想！",
-            info : "醒来吧！！！"
+            info: "醒来吧！！！",
         }
+    },
+    methods: {
+        helloClick() {
+            alert("123")
+        },
+        clickMethod(name) {
+            alert("你好, " + name)
+        },
     },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
     font-weight: normal;
 }
 ul {
