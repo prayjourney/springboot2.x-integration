36f270a601b160291de861fd5c6f63db623574b1
zuiguangyin <2247359268@qq.com>
2020-11-15 23:06:07 +0800

    ❤❤❤样式修改 和v-for使用

--- vue-frontend-collector/hello-quasarvue/src/components/VmodelTest.vue
+++ vue-frontend-collector/hello-quasarvue/src/components/VmodelTest.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="vmodel">
         <!-- 绑定了方法，改变 -->
-        <textarea v-model="poem" @click="print" />
+        <textarea v-model="poem" @click="print" /><br/>
+        <!-- 样式修改 -->
+        <label v-bind:style="style" v-text="text"></label>
+        <!-- v-for的使用 -->
+        <ul>
+            <li v-for="user,index in users" v-bind:key="user.id">
+                {{user.id}} -- {{user.name}} -- {{user.age}}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -11,6 +19,15 @@ export default {
     data() {
         return {
             poem: "helloworld",
+            text: "v-for的使用",
+            style: "font-size:40px",
+            users: [
+                { id: "1", name: "张三", age: "22" },
+                { id: "2", name: "伊能静", age: "50" },
+                { id: "3", name: "黄万里", age: "72" },
+                { id: "4", name: "Jhon MItehds", age: "32" },
+                { id: "5", name: "刘天娥", age: "18" },
+            ],
         }
     },
     methods: {
