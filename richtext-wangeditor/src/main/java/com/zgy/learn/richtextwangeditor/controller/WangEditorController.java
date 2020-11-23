416a364a7b2bbd7a2102eadf5f3bf437c3265e51
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-11-23 23:24:42 +0800

    🎡🎡🎡跳转到页面

--- richtext-wangeditor/src/main/java/com/zgy/learn/richtextwangeditor/controller/WangEditorController.java
+++ richtext-wangeditor/src/main/java/com/zgy/learn/richtextwangeditor/controller/WangEditorController.java
@@ -1,15 +1,19 @@
 package com.zgy.learn.richtextwangeditor.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author renjiaxin
  * @Date 2020/11/23
  * @Description
  */
-@RestController
+@Controller
 @RequestMapping("wangeditor")
 public class WangEditorController {
 
+    @RequestMapping(value = {"", "/", "index"})
+    public String index() {
+        return "index";
+    }
 }
