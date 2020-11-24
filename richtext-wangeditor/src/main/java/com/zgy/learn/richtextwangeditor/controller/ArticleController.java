0294cb26e52e593edc032484a6d406702bbc29fe
zuiguangyin <2247359268@qq.com>
2020-11-25 02:45:56 +0800

    🎳🎳🎳上传图片的后端处理

--- richtext-wangeditor/src/main/java/com/zgy/learn/richtextwangeditor/controller/ArticleController.java
+++ richtext-wangeditor/src/main/java/com/zgy/learn/richtextwangeditor/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.zgy.learn.richtextwangeditor.controller;
 
 import com.zgy.learn.richtextwangeditor.pojo.Article;
 import com.zgy.learn.richtextwangeditor.service.ArticleService;
+import com.zgy.learn.richtextwangeditor.util.ImgInfo;
 import com.zgy.learn.richtextwangeditor.util.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * @Author: renjiaxin
@@ -59,6 +65,39 @@ public class ArticleController {
         }
         log.info("开始获取文章");
         return articleService.selectArticleById(id);
+    }
+
+
+    @PostMapping("/img/upload")
+    @ResponseBody
+    public ImgInfo setImgUrl(@RequestParam("file") MultipartFile file, HttpServletResponse response, HttpServletRequest request)
+            throws Exception {
+        byte[] bytes = file.getBytes();
+        String path = request.getServletContext().getRealPath("/image/");
+        File imgFile = new File(path);
+        if (!imgFile.exists()) {
+            imgFile.mkdirs();
+        }
+        // 文件名称
+        String fileName = file.getOriginalFilename();
+        log.info("img name {}", fileName);
+
+        try (FileOutputStream fos = new FileOutputStream(new File(path + fileName))) {
+            int len = 0;
+            fos.write(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // url设置
+        String value = "http://localhost:8896/image/" + fileName;
+        String[] values = {value};
+
+        ImgInfo imgInfo = new ImgInfo();
+        imgInfo.setError(0);
+        imgInfo.setUrl(values);
+        log.info("img info {}", imgInfo.toString());
+        return imgInfo;
     }
 
 }
---
+++ richtext-wangeditor/src/main/java/com/zgy/learn/richtextwangeditor/util/ImgInfo.java
@@ -0,0 +1,37 @@
+package com.zgy.learn.richtextwangeditor.util;
+
+import java.util.Arrays;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020-11-25 上午 02:17
+ * @Modified by:
+ */
+public class ImgInfo {
+
+    private Integer error;
+    private String[] url;
+
+    public Integer getError() {
+        return error;
+    }
+
+    public void setError(Integer error) {
+        this.error = error;
+    }
+
+    public String[] getUrl() {
+        return url;
+    }
+
+    public void setUrl(String[] url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "ImgInfo [error=" + error + ", url=" + Arrays.toString(url) + "]";
+    }
+
+}
