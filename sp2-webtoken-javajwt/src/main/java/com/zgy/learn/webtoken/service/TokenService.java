719bee6b6c38c082e9e9bb957ae1a672a481f098
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-13 13:46:37 +0800

    🍳🍳🍳生成token的服务

--- sp2-webtoken-javajwt/src/main/java/com/zgy/learn/webtoken/service/TokenService.java
+++ sp2-webtoken-javajwt/src/main/java/com/zgy/learn/webtoken/service/TokenService.java
@@ -6,6 +6,8 @@ import com.zgy.learn.webtoken.pojo.Kid;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author zgy
@@ -14,15 +16,56 @@ import java.util.Date;
  */
 @Service
 public class TokenService {
+    //设置过期时间
+    private static final long EXPIRE_DATE = 30 * 60 * 100000;
 
-    public String getToken(Kid kid) {
+    /**
+     * 简单生成token
+     *
+     * @param kid
+     * @return
+     */
+    public String createToken(Kid kid) {
         Date start = new Date();
-        long currentTime = System.currentTimeMillis() + 60 * 60 * 1000;//一小时有效时间
+        // 一小时有效时间
+        long currentTime = System.currentTimeMillis() + 60 * 60 * 1000;
         Date end = new Date(currentTime);
         String token = "";
 
         token = JWT.create().withAudience(String.valueOf(kid.getId())).withIssuedAt(start).withExpiresAt(end)
                 .sign(Algorithm.HMAC256(kid.getPassword()));
+        return token;
+    }
+
+
+    /**
+     * 完整生成token
+     *
+     * @param kid
+     * @return
+     */
+    public String createTokenComplete(Kid kid) {
+        String token = "";
+
+        try {
+            // 过期时间
+            Date date = new Date(System.currentTimeMillis() + EXPIRE_DATE);
+            // 秘钥及加密算法
+            Algorithm algorithm = Algorithm.HMAC256(kid.getPassword());
+            // 设置头部信息
+            Map<String, Object> header = new HashMap<>();
+            header.put("typ", "JWT");
+            header.put("alg", "HS256");
+            // 携带username, password信息, 生成签名
+            token = JWT.create()
+                    .withHeader(header)
+                    .withClaim("username", kid.getUsername())
+                    .withClaim("password", kid.getPassword()).withExpiresAt(date)
+                    .sign(algorithm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
         return token;
     }
 }
