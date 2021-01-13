2fb6fcf43942f5e1d8f856d26107a54f3302f7eb
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-13 11:32:22 +0800

    🍔🍔🍔java-jwt依赖与自定义的注解

---
+++ sp2-webtoken-javajwt/src/main/java/com/zgy/learn/webtoken/annotation/PassToken.java
@@ -0,0 +1,17 @@
+package com.zgy.learn.webtoken.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author renjiaxin
+ * @Date 2021/1/13
+ * @Description 用来跳过验证的 PassToken
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface PassToken {
+    boolean required() default true;
+}
---
+++ sp2-webtoken-javajwt/src/main/java/com/zgy/learn/webtoken/annotation/UserLoginToken.java
@@ -0,0 +1,17 @@
+package com.zgy.learn.webtoken.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author renjiaxin
+ * @Date 2021/1/13
+ * @Description 用于登录后才能操作的token
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserLoginToken {
+    boolean required() default true;
+}
