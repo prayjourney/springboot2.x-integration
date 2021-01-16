410647e4d5d5e62e12183312aac8b0c2455ab723
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-16 17:27:50 +0800

    🎇🎇🎇测试文件

---
+++ sp2-resources-setting/src/test/java/com/zgy/learn/webtoken/sp2webtokenjjwt/Sp2WebtokenJjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.webtoken.sp2webtokenjjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp2WebtokenJjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
