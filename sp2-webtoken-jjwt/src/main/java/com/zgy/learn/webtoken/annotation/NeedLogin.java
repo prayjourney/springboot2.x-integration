0017f8e40b11c56743a02fcd4456cdd31b402b1d
pray-journey.io <2247359268@qq.com>
2021-01-18 01:47:56 +0800

    🥡🥡🥡两个注解

---
+++ sp2-webtoken-jjwt/src/main/java/com/zgy/learn/webtoken/annotation/NeedLogin.java
@@ -0,0 +1,18 @@
+package com.zgy.learn.webtoken.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author: pray-journey.io
+ * @despcription:
+ * @date: created in 2021-01-17
+ * @modified :
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NeedLogin {
+    boolean required() default true;
+}
---
+++ sp2-webtoken-jjwt/src/main/java/com/zgy/learn/webtoken/annotation/PassLogin.java
@@ -0,0 +1,18 @@
+package com.zgy.learn.webtoken.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author: pray-journey.io
+ * @despcription:
+ * @date: created in 2021-01-18
+ * @modified :
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface PassLogin {
+    boolean required() default true;
+}
