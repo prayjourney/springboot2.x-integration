f3e7edf4fc2a9856c5d444c35c0808d66c5bfeac
pray-journey.io <2247359268@qq.com>
2021-01-18 00:32:05 +0800

    🍕🍕🍕kid实体类与服务

---
+++ sp2-webtoken-jjwt/src/main/java/com/zgy/learn/webtoken/mapper/KidMapper.java
@@ -0,0 +1,16 @@
+package com.zgy.learn.webtoken.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zgy.learn.webtoken.pojo.Kid;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author renjiaxin
+ * @Date 2021/1/13
+ * @Description
+ */
+@Mapper
+@Repository
+public interface KidMapper extends BaseMapper<Kid> {
+}
