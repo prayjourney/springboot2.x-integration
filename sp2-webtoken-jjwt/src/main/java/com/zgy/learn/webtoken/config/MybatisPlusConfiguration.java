ee53b1e1556544637a83a84335f47f95516d6185
pray-journey.io <2247359268@qq.com>
2021-01-18 00:15:59 +0800

    😮😮😮依赖，配置，常规的config

---
+++ sp2-webtoken-jjwt/src/main/java/com/zgy/learn/webtoken/config/MybatisPlusConfiguration.java
@@ -0,0 +1,53 @@
+package com.zgy.learn.webtoken.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author: zgy
+ * @despcription:
+ * @date: Created in 2020/5/18
+ */
+@EnableTransactionManagement
+@MapperScan("com.zgy.learn.webtoken.mapper")
+@Configuration
+public class MybatisPlusConfiguration {
+    // 注册乐观锁插件
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    // 分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+    // 配置逻辑删除组件
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+    // SQL执行效率插件
+    @Bean
+    @Profile(value = {"dev", "test"})
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setFormat(true);
+        // 1ms设置sql执行的最大时间，超过就不去执行
+        performanceInterceptor.setMaxTime(1000);
+        return performanceInterceptor;
+
+    }
+}
