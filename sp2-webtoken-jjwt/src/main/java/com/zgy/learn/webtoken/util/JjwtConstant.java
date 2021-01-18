e7360d0de113b61fd01eb3b3397a7066764140c5
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-18 15:49:13 +0800

    ⚽⚽⚽添加Bearer 协议头

--- sp2-webtoken-jjwt/src/main/java/com/zgy/learn/webtoken/util/JjwtConstant.java
+++ sp2-webtoken-jjwt/src/main/java/com/zgy/learn/webtoken/util/JjwtConstant.java
@@ -14,5 +14,7 @@ public interface JjwtConstant {
     String JWT_SECRET = "abc_helloWorld!!!++==!!!pp12";
     // 上面的JWT_SECRET使用base64转码成下面的JWT_SECRET
     // String JWT_SECRET = "YWJjX2hlbGxvV29ybGQhISErKz09ISEhcHAxMg==";
-    int JWT_TTL = 60 * 60 * 1000;
+    int JWT_TTL = 30 * 60 * 1000;
+
+    String JWT_TOKEN_PREFIX = "Bearer ";
 }
