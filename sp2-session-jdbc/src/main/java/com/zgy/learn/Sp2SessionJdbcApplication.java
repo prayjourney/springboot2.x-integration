0e95c3c887e1771a91558c1f07621b83656e2e6d
pray-journey.io <2247359268@qq.com>
2021-01-24 12:25:01 +0800

    ✨✨✨springsession-jdbc整合init

---
+++ sp2-session-jdbc/src/main/java/com/zgy/learn/Sp2SessionJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp2SessionJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp2SessionJdbcApplication.class, args);
+	}
+
+}
