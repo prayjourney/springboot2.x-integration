7012d43c3bae174fe38a5f75e4259e266d188e3e
pray-journey.io <2247359268@qq.com>
2021-01-24 15:42:23 +0800

    🧦🧦🧦userMapper

--- sp2-session-jdbc/src/main/java/com/zgy/learn/mapper/UserMapper.java
+++ sp2-session-jdbc/src/main/java/com/zgy/learn/mapper/UserMapper.java
@@ -4,8 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zgy.learn.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface UserMapper extends BaseMapper<User> {
     @Select("SELECT * FROM `user` WHERE `id` = #{id}")
     User selectUserById(Integer id);
