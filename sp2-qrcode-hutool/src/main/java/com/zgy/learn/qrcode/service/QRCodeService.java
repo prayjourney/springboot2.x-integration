aa37a517f731711f322a6564c8d444febe7606de
pray-journey.io <2247359268@qq.com>
2021-01-25 23:06:14 +0800

    🎫🎫🎫生成二维码的Service,作用和util一样

---
+++ sp2-qrcode-hutool/src/main/java/com/zgy/learn/qrcode/service/QRCodeService.java
@@ -0,0 +1,64 @@
+package com.zgy.learn.qrcode.service;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.extra.qrcode.QrCodeException;
+import cn.hutool.extra.qrcode.QrCodeUtil;
+import cn.hutool.extra.qrcode.QrConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * @author: pray-journey.io
+ * @despcription:
+ * @date: created in 2021-01-25
+ * @modified :
+ */
+@Service
+@Slf4j
+public class QRCodeService {
+    // 自定义参数，这部分是Hutool工具封装的
+    private static QrConfig initQrConfig() {
+        QrConfig config = new QrConfig(300, 300);
+        // 设置边距，既二维码和背景之间的边距
+        config.setMargin(3);
+        // 设置前景色，既二维码颜色（青色）
+        config.setForeColor(Color.CYAN.getRGB());
+        // 设置背景色（灰色）
+        config.setBackColor(Color.GRAY.getRGB());
+        return config;
+    }
+
+    /**
+     * 生成到文件
+     *
+     * @param content
+     * @param filepath
+     */
+    public void createQRCode2File(String content, String filepath) {
+        try {
+            QrCodeUtil.generate(content, initQrConfig(), FileUtil.file(filepath));
+            log.info("生成二维码成功, 位置在：{}！", filepath);
+        } catch (QrCodeException e) {
+            log.error("发生错误！ {}！", e.getMessage());
+        }
+    }
+
+    /**
+     * 生成到流
+     *
+     * @param content
+     * @param response
+     */
+    public void createQRCode2Stream(String content, HttpServletResponse response) {
+        try {
+            QrCodeUtil.generate(content, initQrConfig(), "png", response.getOutputStream());
+            log.info("生成二维码成功!");
+        } catch (QrCodeException | IOException e) {
+            log.error("发生错误！ {}！", e.getMessage());
+        }
+    }
+}
