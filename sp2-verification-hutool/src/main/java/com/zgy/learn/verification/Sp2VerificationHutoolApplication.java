9602f6d25a25c49ab67e0b591af05df0f852e681
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-25 11:42:35 +0800

    🎃🎃🎃使用hutool工具生成验证码

---
+++ sp2-verification-hutool/src/main/java/com/zgy/learn/verification/Sp2VerificationHutoolApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp2VerificationHutoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp2VerificationHutoolApplication.class, args);
+    }
+
+}
