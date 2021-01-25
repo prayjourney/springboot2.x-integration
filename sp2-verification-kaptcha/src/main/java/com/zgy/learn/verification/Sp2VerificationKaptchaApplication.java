b23c81c079f93364083451c5e9f43da12e681ae0
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-25 17:56:21 +0800

    🍤🍤🍤使用xml生成验证码

--- sp2-verification-kaptcha/src/main/java/com/zgy/learn/verification/Sp2VerificationKaptchaApplication.java
+++ sp2-verification-kaptcha/src/main/java/com/zgy/learn/verification/Sp2VerificationKaptchaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @ImportResource(locations = {"classpath:kaptcha.xml"}) // 导入配置文件, 则注释掉KaptchaConfig即可
 public class Sp2VerificationKaptchaApplication {
 
     public static void main(String[] args) {
