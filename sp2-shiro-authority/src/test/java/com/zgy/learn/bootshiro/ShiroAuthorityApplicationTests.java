33663d73d8d98a21e08b59cdb6104fae69ef4bc4
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-26 11:32:35 +0800

    🍣🍣🍣md5和sha256加密

--- sp2-shiro-authority/src/test/java/com/zgy/learn/bootshiro/ShiroAuthorityApplicationTests.java
+++ sp2-shiro-authority/src/test/java/com/zgy/learn/bootshiro/ShiroAuthorityApplicationTests.java
@@ -1,6 +1,8 @@
 package com.zgy.learn.bootshiro;
 
 
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +11,22 @@ class ShiroAuthorityApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    // MD5不可逆加密
+    @Test
+    public void testMd5Encrypt() {
+        String password = "123";
+        String encodedPassword = new Md5Hash(password).toString();
+        System.out.println("原始密码: " + password + ", md5加密后的密码: " + encodedPassword);
+    }
+
+    // sha256不可逆加密
+    @Test
+    public void testSha256Encrypt() {
+        String password = "123";
+        String encodedPassword = new Sha256Hash(password).toString();
+        System.out.println("原始密码: " + password + ", Sha256加密后的密码: " + encodedPassword);
     }
 
 }
