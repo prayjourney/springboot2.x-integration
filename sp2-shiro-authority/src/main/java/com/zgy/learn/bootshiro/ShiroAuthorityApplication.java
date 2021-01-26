d59a863489bff599057f3cc7e257495375fdc9e6
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-01-26 11:05:53 +0800

    🍛🍛🍛修改和配置

--- sp2-shiro-authority/src/main/java/com/zgy/learn/bootshiro/ShiroAuthorityApplication.java
+++ sp2-shiro-authority/src/main/java/com/zgy/learn/bootshiro/ShiroAuthorityApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootShiroApplication {
+public class ShiroAuthorityApplication {
 
     public static void main(String[] args) {
         System.setProperty("spring.devtools.restart.enabled", "false");
         try {
-            SpringApplication.run(BootShiroApplication.class, args);
+            SpringApplication.run(ShiroAuthorityApplication.class, args);
         } catch (Throwable t) {
             t.printStackTrace();
         }
--- sp2-shiro-authority/src/test/java/com/zgy/learn/bootshiro/ShiroAuthorityApplicationTests.java
+++ sp2-shiro-authority/src/test/java/com/zgy/learn/bootshiro/ShiroAuthorityApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootShiroApplicationTests {
+class ShiroAuthorityApplicationTests {
 
     @Test
     void contextLoads() {
