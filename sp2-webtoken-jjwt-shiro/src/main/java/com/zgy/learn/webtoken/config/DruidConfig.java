095aaf0136939b623510507d1da5f90c78b540ee
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-02-01 15:22:28 +0800

    🍳🍳🍳druid配置

--- sp2-webtoken-jjwt-shiro/src/main/java/com/zgy/learn/webtoken/config/DruidConfig.java
+++ sp2-webtoken-jjwt-shiro/src/main/java/com/zgy/learn/webtoken/config/DruidConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @author renjiaxin
+ * @author z.g.y
  * @date 2021/2/1
  */
 @Configuration
