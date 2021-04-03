ff408cec9c64539766175245ef26996b96a0a951
pray-journey.io <2247359268@qq.com>
2021-04-03 15:10:32 +0800

    🎇🎇🎇初始化

---
+++ sp2-pagination-mybatis/src/main/java/com/zgy/learn/pagination/PaginationMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationMybatisApplication.class, args);
+    }
+
+}
---
+++ sp2-pagination-mybatis/src/test/java/com/zgy/learn/pagination/PaginationMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
