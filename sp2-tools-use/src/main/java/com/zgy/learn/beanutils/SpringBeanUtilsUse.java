6f05c87267d6dcdc80b4fcb850b7a14a065c1fc3
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-05-07 15:55:04 +0800

    ⛰⛰⛰spring beanutils

--- sp2-tools-use/src/main/java/com/zgy/learn/beanutils/SpringBeanUtilsUse.java
+++ sp2-tools-use/src/main/java/com/zgy/learn/beanutils/SpringBeanUtilsUse.java
@@ -14,6 +14,7 @@ public class SpringBeanUtilsUse {
     public static void main(String[] args) {
         Animal animal01 = new Animal().setAge(5).setId(1).setKind("猫").setName("小米").setBirthday(new Date());
         Animal animal02 = new Animal();
+        System.out.println("animal02: " + animal02.toString());
         BeanUtils.copyProperties(animal01, animal02);
         System.out.println("animal01: " + animal01.toString());
         System.out.println(animal01);
