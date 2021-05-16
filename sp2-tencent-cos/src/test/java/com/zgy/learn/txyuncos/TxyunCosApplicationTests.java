06bd493d0964b9bbcddf9f3eed1c19e33d9f66be
pray-journey.io <2247359268@qq.com>
2021-05-16 20:22:30 +0800

    🎃🎃🎃init

---
+++ sp2-tencent-cos/src/test/java/com/zgy/learn/txyuncos/TxyunCosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.txyuncos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxyunCosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
