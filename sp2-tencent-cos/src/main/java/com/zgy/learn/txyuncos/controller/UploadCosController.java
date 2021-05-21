00ba192ed5cb102dfc1852a753ce41df4293153c
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-05-21 15:13:58 +0800

    update

--- sp2-tencent-cos/src/main/java/com/zgy/learn/txyuncos/controller/UploadCosController.java
+++ sp2-tencent-cos/src/main/java/com/zgy/learn/txyuncos/controller/UploadCosController.java
@@ -45,7 +45,7 @@ public class UploadCosController {
         String fileName = file.getOriginalFilename();
         // 1.初始化用户身份信息(secretId, secretKey)
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
-        // 2.设置bucket的区域, COS地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
+        // 2.设置region区域, COS地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
         ClientConfig clientConfig = new ClientConfig(new Region(region));
         // 3.生成cos客户端
         COSClient cosclient = new COSClient(cred, clientConfig);
