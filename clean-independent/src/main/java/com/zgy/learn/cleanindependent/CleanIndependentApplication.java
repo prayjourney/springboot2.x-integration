e6f69a4efb69ff5da54453560386748815b5f904
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-06-16 17:02:59 +0800

    🚁🚁🚁纯净的一个环境，clean-env那个是整个项目的子module

---
+++ clean-independent/src/main/java/com/zgy/learn/cleanindependent/CleanIndependentApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.cleanindependent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanIndependentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanIndependentApplication.class, args);
+    }
+
+}
---
+++ clean-independent/src/test/java/com/zgy/learn/cleanindependent/CleanIndependentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.cleanindependent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanIndependentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
