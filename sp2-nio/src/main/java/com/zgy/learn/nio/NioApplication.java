fd186253abc747ff16c797c273344bf01b7ef0e5
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-06-16 17:16:15 +0800

    ⛅⛅⛅nio project init

---
+++ sp2-nio/src/main/java/com/zgy/learn/nio/NioApplication.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.nio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NioApplication.class, args);
+    }
+
+}
---
+++ sp2-nio/src/test/java/com/zgy/learn/nio/CleanIndependentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn.nio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
