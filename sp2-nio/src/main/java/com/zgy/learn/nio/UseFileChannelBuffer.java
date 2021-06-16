c46fdc7a412dbed99c65fc9d2219555c12d7935d
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-06-17 00:52:04 +0800

    🏩🏩🏩完全使用fileChannel进行拷贝

--- sp2-nio/src/main/java/com/zgy/learn/nio/UseFileChannelBuffer.java
+++ sp2-nio/src/main/java/com/zgy/learn/nio/UseFileChannelBuffer.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * @author: zgy
@@ -13,6 +15,7 @@ import java.nio.channels.FileChannel;
 public class UseFileChannelBuffer {
     public static void main(String[] args) throws IOException {
         useFileChannel01();
+        useFileChannelCopy();
     }
 
     /**
@@ -44,6 +47,33 @@ public class UseFileChannelBuffer {
         outChannel.close();
         fis.close();
         fos.close();
+
+    }
+
+
+    /**
+     * 完全使用fileChannel进行拷贝, StandardOpenOption.XXX是操作模式
+     */
+    public static void useFileChannelCopy() throws IOException {
+        // 1. 获取channel
+        FileChannel inChannel = FileChannel.open(Paths.get("D:/美图/5.05M.jpg"), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get("D:", "美图", "5.05M-copy.jpg"), StandardOpenOption.WRITE,
+                StandardOpenOption.READ, StandardOpenOption.CREATE, StandardOpenOption.CREATE_NEW);
+
+        // 2. 分配缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        // 3. 将通道之中的数据循环读入缓冲区
+        while (inChannel.read(buffer) != -1) {
+            // 4. 切换写模式
+            buffer.flip();
+            // 5. 将缓冲区之中的数据写入通道
+            outChannel.write(buffer);
+            // 6. 清空缓冲区数据, 如果没有读完, 则继续下一轮的读写
+            buffer.clear();
+        }
+        inChannel.close();
+        outChannel.close();
 
     }
 }
