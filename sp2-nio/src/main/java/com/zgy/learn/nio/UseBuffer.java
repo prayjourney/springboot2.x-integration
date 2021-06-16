bee349ed3f4d86b81d2c588d7e15a3d58f4b8298
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-06-16 19:26:52 +0800

    🚋🚋🚋读取缓冲区之中的数据

--- sp2-nio/src/main/java/com/zgy/learn/nio/UseBuffer.java
+++ sp2-nio/src/main/java/com/zgy/learn/nio/UseBuffer.java
@@ -9,10 +9,16 @@ import java.nio.ByteBuffer;
  */
 public class UseBuffer {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        bufferTest();
+        allocateBuffer();
+        readBufferData();
     }
 
-    public static void bufferTest() throws UnsupportedEncodingException {
+    /**
+     * 分配缓冲区
+     *
+     * @throws UnsupportedEncodingException
+     */
+    public static void allocateBuffer() throws UnsupportedEncodingException {
         // 1. 分配一个指定大小的缓冲区
         ByteBuffer buf = ByteBuffer.allocate(1024);
         String str = "Hello world!!!";
@@ -22,4 +28,24 @@ public class UseBuffer {
         System.out.println("limit: " + buf.limit());
         System.out.println("position: " + buf.position());
     }
+
+    /**
+     * 读取缓冲区之中的数据
+     *
+     * @throws UnsupportedEncodingException
+     */
+    public static void readBufferData() throws UnsupportedEncodingException {
+        // 1. 分配指定大小的缓冲区, 非直接缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        String str = "十步杀一人，千里不留行。";
+        // 2. 使用put把数据放入到缓冲区
+        buffer.put(str.getBytes("UTF-8"));
+        // 3. 切换到读数据模式
+        buffer.flip();
+        // 4.使用get获取缓冲区之中的数据
+        byte[] bytes = new byte[buffer.limit()];
+        buffer.get(bytes);
+        System.out.println(new String(bytes, 0, bytes.length));
+    }
+
 }
