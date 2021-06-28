eee405d0450c920943b04781f08fcccdcdb88327
pray-journey.io <2247359268@qq.com>
2021-06-28 22:32:24 +0800

    🎃🎃🎃easypoi使用init

--- sp2-excel-easypoi/src/main/java/com/zgy/learn/easypoi/EasyPoiApplication.java
+++ sp2-excel-easypoi/src/main/java/com/zgy/learn/easypoi/EasyPoiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CleanIndependentApplication {
+public class EasyPoiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CleanIndependentApplication.class, args);
+        SpringApplication.run(EasyPoiApplication.class, args);
     }
 
 }
