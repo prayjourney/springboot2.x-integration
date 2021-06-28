7fd70f539a0bed1048cb45e174d29df8a544542c
pray-journey.io <2247359268@qq.com>
2021-06-28 23:17:19 +0800

    🎃🎃🎃条件检测

--- sp2-excel-easypoi/src/main/java/com/zgy/learn/easypoi/controller/write/WriteExcelController.java
+++ sp2-excel-easypoi/src/main/java/com/zgy/learn/easypoi/controller/write/WriteExcelController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -24,11 +25,20 @@ public class WriteExcelController {
         if (null == student) {
             return "数据无效!";
         }
+        // 存放路径设置
+        String userPath = System.getProperty("user.dir");
+        if (!fileName.endsWith(".xls") && !fileName.endsWith(".xlsx")) {
+            fileName = fileName + ".xlsx";
+        } else {
+            fileName = userPath + File.separator + fileName;
+        }
+
         try {
             writeExcelService.writeObject(student, fileName);
         } catch (Exception e) {
             return e.getMessage();
         }
+
         return "导出完成！";
     }
 
