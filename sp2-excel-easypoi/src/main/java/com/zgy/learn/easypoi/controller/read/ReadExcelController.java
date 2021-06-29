609ee17f5cbf5c73d978cd3a9864d34d2f7f7ab9
zhaoxi-lin.zgy <lin_zhaoxi@sohu.com>
2021-06-29 11:31:23 +0800

    😁😁😁读取所有的行列

--- sp2-excel-easypoi/src/main/java/com/zgy/learn/easypoi/controller/read/ReadExcelController.java
+++ sp2-excel-easypoi/src/main/java/com/zgy/learn/easypoi/controller/read/ReadExcelController.java
@@ -50,7 +50,7 @@ public class ReadExcelController {
      * @throws Exception
      */
     @PostMapping("read-setline")
-    public String readExcel(MultipartFile file, Integer line) throws Exception {
+    public String readExcelSetLine(MultipartFile file, Integer line) throws Exception {
         if (null == file || line < 0) {
             return "输入文件不合法！";
         }
@@ -66,6 +66,31 @@ public class ReadExcelController {
 
         List<PrimaryStudent> result = ExcelImportUtil.importExcel(file.getInputStream(),
                 PrimaryStudent.class, params);
+        for (PrimaryStudent student : result) {
+            System.out.println(student.toString());
+        }
+
+        return "读取完成！";
+    }
+
+    @PostMapping("read-all")
+    public String readExcelAllLine(MultipartFile file) throws Exception {
+        if (null == file) {
+            return "输入文件不合法！";
+        }
+
+        // 导入的参数的设置
+        ImportParams params = new ImportParams();
+        // 设置表头所在的行, 从第1行开始
+        params.setHeadRows(2);
+        // 开始读取的sheet位置, 默认为0
+        params.setStartSheetIndex(0);
+        // 手动控制读取的行数, 不去控制
+        // params.setReadRows(2);
+
+        List<PrimaryStudent> result = ExcelImportUtil.importExcel(file.getInputStream(),
+                PrimaryStudent.class, params);
+        System.out.println("一共读取了：" + result.size() + "行数据！");
         for (PrimaryStudent student : result) {
             System.out.println(student.toString());
         }
