024ab721621d3a8356775b6f55750160b04baf9a
zhaoxi-lin.zgy <2247359268@qq.com>
2021-07-13 15:41:33 +0800

    😄😄😄初始化工程

---
+++ sp2-docker-image/src/test/java/com/zgy/learn/DockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
